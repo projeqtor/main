e21ab158a91ff813bc8a6254c8ddf91af3e9a243
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2010-06-16 10:44:52 +0000

    First commit to SVN - V1.0.0

---
+++ projector/view/preparePreview.php
@@ -0,0 +1,7 @@
+<?php
+   require_once "../tool/projector.php";
+   scriptLog('   ->/view/preparePreview.php'); 
+   header ('Content-Type: text/html; charset=UTF-8');
+?> 
+
+<i><?php echo i18n("messagePreview");?></i>
