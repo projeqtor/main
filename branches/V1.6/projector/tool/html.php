269513d574b8db63268bb5ebd7c65aa71ae65520
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2011-03-09 22:52:34 +0000

    V1.6.0 Commit 4

--- branches/V1.6/projector/tool/html.php
+++ branches/V1.6/projector/tool/html.php
@@ -29,7 +29,7 @@ function htmlDrawOptionForReference($col, $selection, $obj=null, $required=false
     } else if ($col=="idProject") {
       $user=$_SESSION["user"];
       $controlRightsTable=$user->getAccessControlRights();
-	  if (! array_key_exists($obj->getMenuClass(),controlRightsTable)) {
+	  if (! array_key_exists($obj->getMenuClass(),$controlRightsTable)) {
 	    // If AccessRight notdefined for object and user profile => empty list + log error
 		debugLog('Error : no AccessRight for user ' . $user->id . '/' . $user->name . ' and item ' . $obj->getMenuClass());
         return;		
