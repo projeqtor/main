60b94821629d17c34655f2b139b21b45813fe751
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2011-03-22 23:08:15 +0000

    V1.6.0 Commit partiel

--- branches/V1.6/projector/db/Projector_V1.6.0.sql
+++ branches/V1.6/projector/db/Projector_V1.6.0.sql
@@ -9,3 +9,26 @@
 --
 ALTER TABLE `${prefix}work` CHANGE `work` `work` DECIMAL( 5, 2 ) UNSIGNED NULL DEFAULT NULL;
 
+CREATE TABLE `${prefix}list` (
+  `id` int(12) unsigned NOT NULL AUTO_INCREMENT,
+  `list` varchar(100) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `code` varchar(100) DEFAULT NULL,
+  `sortOrder` int(3) DEFAULT NULL,
+  `idle` int(1) unsigned DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+
+INSERT INTO `${prefix}list` (`id`, `list`, `name`, `code`, `sortOrder`, `idle`) VALUES
+(1, 'yesNo', 'displayYes', 'YES', 20, 0),
+(2, 'yesNo', 'displayNo', 'NO', 10, 0);
+
+INSERT INTO `${prefix}habilitationother` (`idProfile`, `scope`, `rightAccess`) VALUES
+(1, 'combo', 1),
+(2, 'combo', 2),
+(3, 'combo', 1),
+(4, 'combo', 2),
+(6, 'combo', 2),
+(7, 'combo', 2),
+(5, 'combo', 2);
+
