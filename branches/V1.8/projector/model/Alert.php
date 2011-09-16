b1301d1ecb59ec1a63f2b4f2f6b30e5b188cf7f1
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2011-09-16 20:07:37 +0000

    V1.8.0

--- branches/V1.8/projector/model/Alert.php
+++ branches/V1.8/projector/model/Alert.php
@@ -13,28 +13,33 @@ class Alert extends SqlElement {
   public $idIndicatorValue;
   public $idUser;
   public $alertType;
-  public $alertDateTime;
   public $alertInitialDateTime;
-  public $alertReadDateTime; 
-  public $title;
-  public $message;
+  public $alertDateTime;
   public $readFlag;
+  public $alertReadDateTime; 
+  public $_spe_markAsRead;
   public $idle;
-  public $_col_2_2;
-  
+  public $_col_2_2_Message;
+  public $title;
+  public $message;  
   // Define the layout that will be used for lists
   
-  private static $_fieldsAttributes=array("idIndicatorValue"=>"hidden");
+  private static $_fieldsAttributes=array("idIndicatorValue"=>"hidden",
+                                          "readFlag"=>"nobr", 
+                                          "refType"=>"display,nobr", 
+                                          "refId"=>"display");
   
-    private static $_colCaptionTransposition = array('alertType'=>'type');
+    private static $_colCaptionTransposition = array('alertType'=>'type',
+                                                     'refType'=>'element',
+                                                     'idUser'=>'alertReceiver');
     
   private static $_layout='
     <th field="id" formatter="numericFormatter" width="5%" ># ${id}</th>
     <th field="nameProject" width="10%" >${idProject}</th>
-    <th field="nameUser" width="10%" >${idUser}</th>
+    <th field="nameUser" width="10%" >${alertReceiver}</th>
     <th field="refType" width="10%" formatter="translatterFormatter" >${element}</th>
     <th field="refId" width="5%" >${id}</th>
-    <th field="alertType" width="10%">${idType}</th>
+    <th field="alertType" width="10%">${type}</th>
     <th field="title" width="40%" >${title}</th>
     <th field="readFlag" width="5%" formatter="booleanFormatter" >${readFlag}</th>
     <th field="idle" width="5%" formatter="booleanFormatter" >${idle}</th>
@@ -84,5 +89,21 @@ class Alert extends SqlElement {
   protected function getStaticColCaptionTransposition($fld) {
     return self::$_colCaptionTransposition;
   }
+    
+  public function drawSpecificItem($item){
+    $result="";
+    if ($item=='markAsRead') {
+    	$user=$_SESSION['user'];
+    	if ($user->id==$this->idUser and ! $this->readFlag) {
+    	  $result .='<table><tr><td class="label">&nbsp;</td><td>';
+        $result .='<button dojoType="dijit.form.Button" onclick="setAlertReadMessage();">';
+        $result .= i18n("markAsRead");
+        $result .='</button>';
+        $result .='</td></tr></table>';
+    	}
+    }
+    return $result;
+  }
+  
 }
 ?>
