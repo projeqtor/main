241b803f72675e544ee210c8246114a260453dd4
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2011-10-22 23:34:48 +0000

    V1.8.1 to V1.9.0

--- branches/V1.9/projector/tool/jsonQuery.php
+++ branches/V1.9/projector/tool/jsonQuery.php
@@ -284,7 +284,7 @@
          . ' from ' . $queryFrom
          . ' where ' . $queryWhere 
          . ' order by' . $queryOrderBy;
-debugLog($query);
+//debugLog($query);
     $result=Sql::query($query);
     $nbRows=0;
     if ($print) {
