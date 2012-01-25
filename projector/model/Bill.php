fccf9e46bfb302fed68ec52191ba538374829573
babynus <support@projeqtor.org>
2012-01-25 17:33:28 +0000

    V1.9.1

--- projector/model/Bill.php
+++ projector/model/Bill.php
@@ -31,10 +31,10 @@ class Bill extends SqlElement {
   // Define the layout that will be used for lists
   private static $_layout='
     <th field="id" formatter="numericFormatter" width="5%" ># ${id}</th>
-    <th field="name" width="20%" >${name}</th>
-    <th field="date" formatter="dateFormatter" width="10%" >${date}</th>
-    <th field="nameProject" width="15%" >${idProject}</th>
     <th field="nameClient" width="15%" >${idClient}</th>
+    <th field="nameProject" width="15%" >${idProject}</th>
+    <th field="name" width="20%" >${name}</th>
+    <th field="date" formatter="dateFormatter" width="10%" >${date}</th>  
     <th field="nameRecipient" width="15%" >${idRecipient}</th>
     <th field="fullAmount" width="10%" >${fullAmount}</th>
     <th field="billId" width="5%" >${billId}</th>
