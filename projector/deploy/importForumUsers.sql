12b6c16f517bb514eacad85dd7e3660e674737ea
babynus <support@projeqtor.org>
2012-02-26 13:17:18 +0000

    V2.0.1

---
+++ projector/deploy/importForumUsers.sql
@@ -0,0 +1,9 @@
+-- Run this query in _projectorForum database
+
+SELECT * from not_banned_users;
+
+-- Export to csv file format (, separator)
+
+-- Import in Actimail users list
+
+-- Also import Joola users... 
