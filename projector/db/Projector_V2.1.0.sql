223cb5ddec3cd758b0054ef900106269d6062444
babynus <support@projeqtor.org>
2012-03-03 20:22:14 +0000

    V2.1.0

--- projector/db/Projector_V2.1.0.sql
+++ projector/db/Projector_V2.1.0.sql
@@ -17,7 +17,22 @@ ALTER TABLE `${prefix}planningelement` CHANGE initialWork initialWork DECIMAL(14
  
 ALTER TABLE `${prefix}product` ADD COLUMN `idProduct`  int(12) unsigned DEFAULT NULL;
 
-ALTER TABLE `${prefix}ticket` ADD COLUMN `plannedWork`  DECIMAL(6,2) unsigned default 0,
-ADD COLUMN `realWork`  DECIMAL(6,2) unsigned DEFAULT 0;
+--ALTER TABLE `${prefix}ticket` ADD COLUMN `plannedWork`  DECIMAL(6,2) unsigned default 0,
+--ADD COLUMN `realWork`  DECIMAL(6,2) unsigned DEFAULT 0;
 
 ALTER TABLE `${prefix}ticket` ADD COLUMN `idTicket` int(12) unsigned DEFAULT NULL;
+
+CREATE TABLE `${prefix}workelement` (
+  `id` int(12) unsigned NOT NULL AUTO_INCREMENT,
+  `refType` varchar(100) DEFAULT NULL,
+  `refId` int(12) unsigned DEFAULT NULL,
+  `refName` varchar(100) DEFAULT NULL,
+  `plannedWork`  DECIMAL(6,2) unsigned default 0,
+  `realWork`  DECIMAL(6,2) unsigned DEFAULT 0,
+  `leftWork`  DECIMAL(6,2) unsigned DEFAULT 0,
+  `done` int(1) unsigned DEFAULT '0',
+  `idle` int(1) unsigned DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=innoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+ALTER TABLE `${prefix}workelement` ADD INDEX workelementReference (refType, refId);
