98c21903835a0abde7c0224069297eee01676566
babynus <support@projeqtor.org>
2012-03-19 00:04:02 +0000

    V2.1.0 manual

--- projector/manual/manual.php
+++ projector/manual/manual.php
@@ -142,7 +142,7 @@
           </tr>
           <tr>
             <td colspan="3">
-              <img src="slides/img<?php echo $page;?>.png" />
+              <img src="slides/Diapositive<?php echo $page;?>.PNG" />
             </td>
           </tr>
         </table>
