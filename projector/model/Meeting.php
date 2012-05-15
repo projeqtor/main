c7c68cec67ef9500cd20d34122b49f3ac430f829
babynus <support@projeqtor.org>
2012-05-15 19:37:09 +0000

    V2.2.3

--- projector/model/Meeting.php
+++ projector/model/Meeting.php
@@ -352,6 +352,7 @@ class Meeting extends SqlElement {
     }
 
     $result=sendMail($destList, $this->name, $vcal, $this, $headers,$sender);
+    if (! $result) {$sent=0;}
     return $sent;
 
   }
