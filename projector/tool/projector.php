0e0d0bfc585f6572c959d8e18ab5220a002f6d86
babynus <support@projeqtor.org>
2012-05-16 20:33:48 +0000

    V2.1.3

--- projector/tool/projector.php
+++ projector/tool/projector.php
@@ -53,8 +53,8 @@ $cr="\n";                     // Line feed (just for html dynamic building, to e
 // === Application data : version, dependencies, about message, ...
 $applicationName="Project'Or RIA"; // Name of the application
 $copyright=$applicationName;  // Copyright to be displayed
-$version="V2.1.2";            // Version of application : Major / Minor / Release
-$build="0047.1";                // Build number. To be increased on each release
+$version="V2.1.3";            // Version of application : Major / Minor / Release
+$build="0047.2";                // Build number. To be increased on each release
 $website="http://projectorria.toolware.fr"; // ProjectOr site url
 $aboutMessage='';             // About message to be displayed when clicking on application logo
 $aboutMessage.='<div>' . $applicationName . ' ' . $version . '</div><br/>';
