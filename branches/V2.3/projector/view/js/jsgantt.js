7b0c3645b8150f5e406118f66c668b15b842cdcf
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2012-05-27 23:12:31 +0000

    V2.3.0

--- branches/V2.3/projector/view/js/jsgantt.js
+++ branches/V2.3/projector/view/js/jsgantt.js
@@ -101,7 +101,12 @@ JSGantt.TaskItem = function(pID, pName, pStart, pEnd, pColor, pLink, pMile, pRes
       vDuration = tmpPer + ' ' + i18n('shortMinute');
     } else {
       if (this.getStart()==null || this.getEnd()==null) {
-        vDuration = '-';
+    	if (this.getStart()==null && this.getRealEnd()==null) {
+    	  vDuration = '-';
+    	} else {
+    	  tmpPer =  workDayDiffDates(this.getStart(), this.getRealEnd());
+          vDuration = tmpPer + ' ' + i18n('shortDay');
+    	}
       } else {
         tmpPer =  workDayDiffDates(this.getStart(), this.getEnd());
         vDuration = tmpPer + ' ' + i18n('shortDay');
