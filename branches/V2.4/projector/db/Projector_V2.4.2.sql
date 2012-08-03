18eb177325f5bad7ad44b4524149bdac1aeca836
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2012-08-03 16:06:26 +0000

    V2.4.2

--- branches/V2.4/projector/db/Projector_V2.4.2.sql
+++ branches/V2.4/projector/db/Projector_V2.4.2.sql
@@ -12,3 +12,9 @@ INSERT INTO `${prefix}originable` (`id`, `name`, `idle`) VALUES
 (14, 'Requirement', 0),
 (15, 'TestSession', 0),
 (16, 'TestCase', 0);
+
+ALTER TABLE `${prefix}requirement` ADD COLUMN `idRunStatus` int(12) unsigned default null;
+ALTER TABLE `${prefix}testsession` ADD COLUMN `idRunStatus` int(12) unsigned default null;
+
+INSERT INTO `${prefix}runstatus` (id, name, color, sortOrder, idle) VALUES
+(5, 'empty', '#FF00A5', 500, 0);
