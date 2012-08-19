473ea58e1dce8bb0eeafb6cd64605ed82f99db75
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2012-08-19 22:44:33 +0000

    V2.5

--- branches/V2.5/projector/tool/saveCollapsed.php
+++ branches/V2.5/projector/tool/saveCollapsed.php
@@ -4,6 +4,7 @@
  */
 require_once "../tool/projector.php";
 
+Sql::beginTransaction();
 $scope=$_REQUEST['scope'];
 $value=$_REQUEST['value'];
 if ($value=='true') {
@@ -11,5 +12,5 @@ if ($value=='true') {
 } else {
   Collapsed::expand($scope);
 }
-
+Sql::commitTransaction();
 ?>
