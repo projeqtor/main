927f9cd64c69576608eeeb85eea37682953774bb
babynus <support@projeqtor.org>
2012-10-14 16:18:43 +0000

    V3.0

---
+++ branches/V3.0/projector/external/dojo/cldr/nls/ar/currency.js
@@ -0,0 +1,2 @@
+//>>built
+define("dojo/cldr/nls/ar/currency",{"AUD_displayName":"دولار أسترالي","CAD_displayName":"دولار كندي","CHF_displayName":"فرنك سويسري","CNY_displayName":"يوان صيني","CNY_symbol":"ي.ص","EUR_displayName":"يورو","GBP_displayName":"جنيه إسترليني","HKD_displayName":"دولار هونج كونج","JPY_displayName":"ين ياباني","USD_displayName":"دولار أمريكي"});
