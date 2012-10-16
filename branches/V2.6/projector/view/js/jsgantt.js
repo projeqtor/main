e0e3ccbf1890d386bfded8508b2b12c885541095
babynus <support@projeqtor.org>
2012-10-16 20:15:30 +0000

    2.5.3

--- branches/V2.6/projector/view/js/jsgantt.js
+++ branches/V2.6/projector/view/js/jsgantt.js
@@ -446,7 +446,7 @@ JSGantt.GanttChart =  function(pGanttVar, pDiv, pFormat) {
       + ( (1+vWorkWidth) * this.getShowRealWork() )
       + ( (1+vWorkWidth) * this.getShowValidatedWork() );
     var vRightWidth = vWidth - vLeftWidth - 18;
-    var ffSpecificHeight=(dojo.isFF)?' class="ganttHeight"':'';
+    var ffSpecificHeight=(dojo.isFF<16)?' class="ganttHeight"':'';
     var vLeftTable="";
     var vRightTable="";
     var vTopRightTable="";
@@ -589,13 +589,14 @@ JSGantt.GanttChart =  function(pGanttVar, pDiv, pFormat) {
         }
         vID = vTaskList[i].getID();
         var invisibleDisplay=(vTaskList[i].getVisible() == 0)?'style="display:none"':'';
-        vLeftTable += '<TR id=child_' + vID + ' class="dojoDndItem ganttTask' + vRowType + '" ' 
+        vLeftTable += '<TR id=child_' + vID + 'class="dojoDndItem ganttTask' + vRowType + '" ' 
           + invisibleDisplay
           + ' xonMouseover=JSGantt.ganttMouseOver(' + vID + ',"left","' + vRowType + '")'
           + ' xonMouseout=JSGantt.ganttMouseOut(' + vID + ',"left","' + vRowType + '")>' ;
         vLeftTable += '  <TD class="ganttName" style="width:16px">'
-          +'<img style="width:16px" src="css/images/icon' 
-          + vTaskList[i].getClass() + '16.png" /></TD>'
+        //  +'<img style="width:16px" src="css/images/icon' 
+        //  + vTaskList[i].getClass() + '16.png" />'
+        + '</TD>'
           +'<TD class="ganttName ganttAlignLeft" style="width: ' + vNameWidth + 'px;" nowrap>';
         vLeftTable += '<div style="width: ' + vNameWidth + 'px;">';
         var levl=vTaskList[i].getLevel();
