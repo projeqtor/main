6820993e6484b27d75ee15b7c714a9f94b89c9bc
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2012-11-11 00:08:16 +0000

    V3.0.0

--- branches/V3.0/projector/files/config/parameters.php
+++ branches/V3.0/projector/files/config/parameters.php
@@ -1,15 +1,15 @@
 <?php
 // =======================================================================================
 // Automatically generated parameter file
-// on 2012-11-10 13:05:35
+// on 2012-11-10 21:22:37
 // =======================================================================================
 $paramDbType='pgsql';
 $paramDbHost='localhost';
 $paramDbPort='5432';
 $paramDbUser='postgres';
 $paramDbPassword='admin31!';
-$paramDbName='projectorria';
-$paramDbPrefix='v30_';
+$paramDbName='projectorria_v30';
+$paramDbPrefix='';
 $logFile='../files/logs/projector_${date}.log';
 $logLevel='3';
 $paramDebugMode='false';
