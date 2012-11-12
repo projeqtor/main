e7f97d2e673ad7f5e04ca9a16fcd5f67e738d1d2
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2012-11-12 00:03:49 +0000

    V3.0.0

--- branches/V3.0/projector/report/history.php
+++ branches/V3.0/projector/report/history.php
@@ -26,8 +26,14 @@ if ($scope=="deleted") {
   $where.= " operation='delete' ";
   $where.= " and refType in ('Ticket','Activity','Milestone', 'Risk', 'Action', 'Issue', 'Meeting', 'Decision', 'Question', 'Project' )";
 } else {
-  $where = " refType='$refType' and refId=".Sql::fmtId($refId);
+  $where = " (refType, refId) in ( ('$refType',".Sql::fmtId($refId).")";
   $obj=new $refType($refId);
+  foreach ($obj as $fld=>$val) {
+  	if (is_object($val) and isset($val->id)) {
+  		$where.=", ('$fld',".Sql::fmtId($val->id).")";
+  	}
+  }
+  $where .= ")";
 }
 
 $order = ' operationDate desc, id asc';
@@ -55,27 +61,61 @@ $stockDate=null;
 $stockUser=null;
 $stockOper=null;
 foreach($historyList as $hist) {
+	if (substr($hist->colName,0,25)=='subDirectory|Attachement|' 
+	 or substr($hist->colName,0,19)=='idTeam|Attachement|') {
+		continue;
+	}
   $colName=($hist->colName==null)?'':$hist->colName;
+  $split=explode('|', $colName);
+  if (count($split)==3) {
+    $colName=$split[0];
+    $refType=$split[1];
+    $refId=$split[2];
+    $refObject='';
+  } else if (count($split)==4) {
+    $refObject=$split[0];
+    $colName=$split[1];
+    $refType=$split[2];
+    $refId=$split[3];
+  } else {
+    $refType='';
+    $refId='';
+    $refObject='';
+  }
   $curObj=null; $dataType=""; $dataLength=0;
   $hide=false;
   $oper=i18n('operation' . ucfirst($hist->operation) );
-  $userId=$hist->idUser;
-  $userName=SqlList::getNameFromId('User',$userId);
+  $user=$hist->idUser;
+  $user=SqlList::getNameFromId('User',$user);
   $date=htmlFormatDateTime($hist->operationDate);
   $class="NewOperation";
   if ($stockDate==$hist->operationDate 
   and $stockUser==$hist->idUser
   and $stockOper==$hist->operation) {
     $oper="";
-    $userName="";
-    $userId="";
+    $user="";
     $date="";
     $class="ContinueOperation";
   }
-  if ($colName!='') {
-    $curObj=new $hist->refType();
+  if ($colName!='' or $refType!="") {
+    if ($refType) {
+      if ($refType=="TestCase") {
+        $curObj=new TestCaseRun();
+      } else {
+        $curObj=new $refType();
+      }
+    } else {
+      $curObj=new $hist->refType();
+    }
     if ($curObj) {
-      $colCaption=$curObj->getColCaption($hist->colName);
+      if ($refType) {
+        $colCaption=i18n($refType). ' #' . $refId . ' ' . $curObj->getColCaption($colName);
+        if ($refObject) {
+          $colCaption=i18n($refObject) . ' - ' . $colCaption;
+        }
+      } else {
+        $colCaption=$curObj->getColCaption($colName);
+      }
       $dataType=$curObj->getDataType($colName);
       $dataLength=$curObj->getDataLength($colName);
       if (strpos($curObj->getFieldAttributes($colName), 'hidden')!==false) {
@@ -90,38 +130,45 @@ foreach($historyList as $hist) {
   }
   if (! $hide) {
     echo '<tr>';
-    echo '<td class="historyData'. $class .'" style="width:15%">' . $date . '</td>';
-    echo '<td class="historyData'. $class .'" style="width:10%">' . $userName . '</td>';
-    if ($scope=='deleted') {
-      echo '<td class="historyData'. $class .'">' . i18n($hist->refType) . ' #' . $hist->refId . '</td>';
-    } else {
-      echo '<td class="historyData'. $class .'" style="width:10%">' . $oper . '</td>';
-      
-      echo '<td class="historyData" style="width:15%">' . $colCaption . '</td>';
-      $oldValue=$hist->oldValue;
-      $newValue=$hist->newValue;
-      if ($dataType=='int' and $dataLength==1) { // boolean
-        $oldValue=htmlDisplayCheckbox($oldValue);
-        $newValue=htmlDisplayCheckbox($newValue);
-      } else if (substr($colName,0,2)=='id' and strlen($colName)>2
-                 and strtoupper(substr($colName,2,1))==substr($colName,2,1)) {
-        if ($oldValue!=null and $oldValue!='') {
+    echo '<td class="historyData'. $class .'" width="10%">' . $oper . '</td>';      
+    echo '<td class="historyData" width="14%">' . $colCaption . '</td>';
+    $oldValue=$hist->oldValue;
+    $newValue=$hist->newValue;
+    if ($dataType=='int' and $dataLength==1) { // boolean
+      $oldValue=htmlDisplayCheckbox($oldValue);
+      $newValue=htmlDisplayCheckbox($newValue);
+    } else if (substr($colName,0,2)=='id' and strlen($colName)>2
+               and strtoupper(substr($colName,2,1))==substr($colName,2,1)) {
+      if ($oldValue!=null and $oldValue!='') {
+        if ($oldValue==0 and $colName=='idStatus') {
+          $oldValue='';
+        } else {
           $oldValue=SqlList::getNameFromId(substr($colName,2),$oldValue);
         }
-        if ($newValue!=null and $newValue!='') {
-          $newValue=SqlList::getNameFromId(substr($colName,2),$newValue);
-        }
-      } else if ($colName=="color") {
-        $oldValue=htmlDisplayColored("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;",$oldValue);
-        $newValue=htmlDisplayColored("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;",$newValue);
-      } else {
-        $oldValue=htmlEncode($oldValue,'print');
-        $newValue=htmlEncode($newValue,'print');
       }
-      
-      echo '<td class="historyData" style="width:25%">' . $oldValue . '</td>';
-      echo '<td class="historyData" style="width:25%">' . $newValue . '</td>';
-    }
+      if ($newValue!=null and $newValue!='') {
+        $newValue=SqlList::getNameFromId(substr($colName,2),$newValue);
+      }
+    } else if ($colName=="color") {
+      $oldValue=htmlDisplayColored("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;",$oldValue);
+      $newValue=htmlDisplayColored("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;",$newValue);
+    } else if ($dataType=='date') {
+      $oldValue=htmlFormatDate($oldValue);
+      $newValue=htmlFormatDate($newValue);
+    } else if ($dataType=='datetime') {
+      $oldValue=htmlFormatDateTime($oldValue);
+      $newValue=htmlFormatDateTime($newValue);
+    } elseif ($dataType=='decimal' and substr($colName, -4,4)=='Work') {
+      $oldValue = Work::displayWork($oldValue) . ' ' . Work::displayShortWorkUnit();
+      $newValue = Work::displayWork($newValue) . ' ' . Work::displayShortWorkUnit();
+    } else {
+      $oldValue=htmlEncode($oldValue,'print');
+      $newValue=htmlEncode($newValue,'print');
+    }    
+    echo '<td class="historyData" width="23%">' . $oldValue . '</td>';
+    echo '<td class="historyData" width="23%">' . $newValue . '</td>';
+    echo '<td class="historyData'. $class .'" width="15%">' . $date . '</td>';
+    echo '<td class="historyData'. $class .'" width="15%">' . $user . '</td>';
     echo '</tr>';
     $stockDate=$hist->operationDate;
     $stockUser=$hist->idUser;
