d4950f5e6dc572b4cc81be1ea1a33cd82a72c1de
babynus <support@projeqtor.org>
2012-12-03 22:49:58 +0000

    V3.1

--- projector/files/config/parameters.php
+++ projector/files/config/parameters.php
@@ -6,7 +6,7 @@
 //$paramDbType='pgsql'; $paramDbPort='5432'; $paramDbUser='postgres'; $paramDbPassword='admin31!';
 $paramDbType='mysql'; $paramDbPort='3306'; $paramDbUser='root'; $paramDbPassword='mysql';
 $paramDbHost='127.0.0.1'; 
-$paramDbName='projectorria_v30';
+$paramDbName='projectorria_v31';
 $paramDbPrefix='';
 $logFile='../files/logs/projector_${date}.log';
 $logLevel='3';
