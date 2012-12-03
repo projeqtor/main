813ca9a070e3a301633cff49cd25bbab4a586348
babynus <support@projeqtor.org>
2012-12-03 16:07:57 +0000

    V3.1

---
+++ branches/V3.1/projector/external/dojo/cldr/nls/ko/number.js
@@ -0,0 +1,2 @@
+//>>built
+define("dojo/cldr/nls/ko/number",{"decimal":".","group":",","list":";","percentSign":"%","plusSign":"+","minusSign":"-","exponential":"E","perMille":"‰","infinity":"∞","nan":"NaN","decimalFormat":"#,##0.###","decimalFormat-short":"000조","scientificFormat":"#E0","percentFormat":"#,##0%","currencyFormat":"¤#,##0.00"});
---
+++ branches/V3.1/projector/files/config/parameters.php
@@ -0,0 +1,14 @@
+<?php
+// =======================================================================================
+// Automatically generated parameter file
+// on 2012-11-10 21:22:37
+// =======================================================================================
+//$paramDbType='pgsql'; $paramDbPort='5432'; $paramDbUser='postgres'; $paramDbPassword='admin31!';
+$paramDbType='mysql'; $paramDbPort='3306'; $paramDbUser='root'; $paramDbPassword='mysql';
+$paramDbHost='127.0.0.1'; 
+$paramDbName='projectorria_v30';
+$paramDbPrefix='';
+$logFile='../files/logs/projector_${date}.log';
+$logLevel='3';
+$paramDebugMode='false';
+//======= END
