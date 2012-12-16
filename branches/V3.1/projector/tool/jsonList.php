eef3f9882e366b1535f90362cde0c51ec76b1612
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2012-12-16 18:11:19 +0000

    V3.1.0

--- branches/V3.1/projector/tool/jsonList.php
+++ branches/V3.1/projector/tool/jsonList.php
@@ -32,6 +32,7 @@
         echo ' {id:"=", name:"="}';
         echo ',{id:">=", name:">="}';
         echo ',{id:"<=", name:"<="}';
+        echo ',{id:"<>", name:"<>"}';
         if ($dataType!='int' and $dataType!='decimal') {
           //echo ',{id:"xx", name:"xx"}';
           echo ',{id:"<=now+", name:"<= ' . i18n('today') . ' + "}';
@@ -42,6 +43,7 @@
         echo ',{id:"SORT", name:"' . i18n('sortFilter') .'"}';
       } else if ($dataType=='varchar') {
         echo ' {id:"LIKE", name:"' . i18n("contains") . '"}';
+        echo ',{id:"NOT LIKE", name:"' . i18n("notContains") . '"}';
         echo ',{id:"isEmpty", name:"' . i18n('isEmpty') . '"}';
         echo ',{id:"isNotEmpty", name:"' . i18n('isNotEmpty') . '"}';
         echo ',{id:"SORT", name:"' . i18n('sortFilter') .'"}';
@@ -50,6 +52,7 @@
         echo ',{id:"SORT", name:"' . i18n('sortFilter') .'"}';
       } else if ($dataType=='list') {
         echo ' {id:"IN", name:"' . i18n("amongst") . '"}';
+        echo ',{id:"NOT IN", name:"' . i18n("notAmongst") . '"}';
         echo ',{id:"isEmpty", name:"' . i18n('isEmpty') . '"}';
         echo ',{id:"isNotEmpty", name:"' . i18n('isNotEmpty') . '"}';
         echo ',{id:"SORT", name:"' . i18n('sortFilter') .'"}';
