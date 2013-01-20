d2837a26144bfd368afe9473a173203bac7c25a7
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2013-01-20 12:07:10 +0000

    3.0.2 parameters

--- branches/V3.0/projector/files/config/parameters.php
+++ branches/V3.0/projector/files/config/parameters.php
@@ -3,10 +3,13 @@
 // Automatically generated parameter file
 // on 2012-11-10 21:22:37
 // =======================================================================================
-//$paramDbType='pgsql'; $paramDbPort='5432'; $paramDbUser='postgres'; $paramDbPassword='admin31!';
-$paramDbType='mysql'; $paramDbPort='3306'; $paramDbUser='root'; $paramDbPassword='mysql';
+$paramDbType='pgsql'; $paramDbPort='5432'; $paramDbUser='postgres'; $paramDbPassword='admin31!';
+//$paramDbType='pgsql'; $paramDbPort='5432'; $paramDbUser='santelectureseule'; $paramDbPassword='pwdSeuleLectureSante';
+//$paramDbHost='88.190.41.216'; 
+$paramDbName='sante';
+//$paramDbType='mysql'; $paramDbPort='3306'; $paramDbUser='root'; $paramDbPassword='mysql';
 $paramDbHost='127.0.0.1'; 
-$paramDbName='projectorria_v30';
+//$paramDbName='projector_v30';
 $paramDbPrefix='';
 $logFile='../files/logs/projector_${date}.log';
 $logLevel='3';
