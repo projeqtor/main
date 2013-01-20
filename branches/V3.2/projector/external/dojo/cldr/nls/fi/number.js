fcb7b4c9236da1b6903c93ee82c4f4b9a7b0a425
babynus <support@projeqtor.org>
2013-01-20 18:03:52 +0000

    V3.2

---
+++ branches/V3.2/projector/external/dojo/cldr/nls/fi/number.js
@@ -0,0 +1,2 @@
+//>>built
+define("dojo/cldr/nls/fi/number",{"decimal":",","group":" ","list":";","percentSign":"%","plusSign":"+","minusSign":"-","exponential":"E","perMille":"‰","infinity":"∞","nan":"epäluku","decimalFormat":"#,##0.###","decimalFormat-short":"000 b","scientificFormat":"#E0","percentFormat":"#,##0 %","currencyFormat":"#,##0.00 ¤"});
