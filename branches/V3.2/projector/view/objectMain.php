3a153844b95c1111b5c2e5a5d5e8be53ae927983
babynus <support@projeqtor.org>
2013-01-28 20:00:05 +0000

    V3.2

--- branches/V3.2/projector/view/objectMain.php
+++ branches/V3.2/projector/view/objectMain.php
@@ -6,7 +6,7 @@
   scriptLog('   ->/view/objectMain.php');  
 ?>
 <div id="mainDivContainer" class="container" dojoType="dijit.layout.BorderContainer" liveSplitters="false">
-  <div id="listDiv" dojoType="dijit.layout.ContentPane" region="top" splitter="true">
+  <div id="listDiv" dojoType="dijit.layout.ContentPane" region="top" splitter="true" style="height:400px">
    <?php include 'objectList.php'?>
   </div>
   <div id="detailDiv" dojoType="dijit.layout.ContentPane" region="center" >
