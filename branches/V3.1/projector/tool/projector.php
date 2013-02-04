d3b134ea8093144d9237f6cebd9d0771d42d5b64
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2013-02-04 17:12:57 +0000

    V3.1.3

--- branches/V3.1/projector/tool/projector.php
+++ branches/V3.1/projector/tool/projector.php
@@ -56,8 +56,8 @@ $cr="\n";                     // Line feed (just for html dynamic building, to e
 // === Application data : version, dependencies, about message, ...
 $applicationName="Project'Or RIA"; // Name of the application
 $copyright=$applicationName;  // Copyright to be displayed
-$version="V3.1.2";            // Version of application : Major / Minor / Release
-$build="0070";                // Build number. To be increased on each release
+$version="V3.1.3";            // Version of application : Major / Minor / Release
+$build="0071";                // Build number. To be increased on each release
 $website="http://projectorria.toolware.fr"; // ProjectOr site url
 $aboutMessage='';             // About message to be displayed when clicking on application logo
 $aboutMessage.='<div>' . $applicationName . ' ' . $version . '</div><br/>';
