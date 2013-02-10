41a09462708033fa24c0566f8bfbd36e5d848cc6
babynus <support@projeqtor.org>
2013-02-10 12:52:59 +0000

    3.0.3 ?

--- projector/files/config/parameters.php
+++ projector/files/config/parameters.php
@@ -3,14 +3,18 @@
 // Automatically generated parameter file
 // on 2012-11-10 21:22:37
 // =======================================================================================
-$paramDbType='pgsql'; $paramDbPort='5432'; $paramDbUser='postgres'; $paramDbPassword='admin31!';
-//$paramDbType='pgsql'; $paramDbPort='5432'; $paramDbUser='santelectureseule'; $paramDbPassword='pwdSeuleLectureSante';
-//$paramDbHost='88.190.41.216'; 
-$paramDbName='sante';
-//$paramDbType='mysql'; $paramDbPort='3306'; $paramDbUser='root'; $paramDbPassword='mysql';
+//
+// MySql or PostgresSql
+//$paramDbType='pgsql'; $paramDbPort='5432'; $paramDbUser='postgres'; $paramDbPassword='admin31!';
+$paramDbType='mysql'; $paramDbPort='3306'; $paramDbUser='root'; $paramDbPassword='mysql';
+//
+// Database
+$paramDbName='projector_v30';$paramDbPrefix='';
+//$paramDbName='sante'; $paramDbPrefix='';
+//$paramDbName='hosted_galaxysemi'; $paramDbPrefix='galaxysemi_';
+//
+// Fixed
 $paramDbHost='127.0.0.1'; 
-//$paramDbName='projector_v30';
-$paramDbPrefix='';
 $logFile='../files/logs/projector_${date}.log';
 $logLevel='3';
 $paramDebugMode='false';
