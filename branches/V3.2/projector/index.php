9fc807e75b3cf1c3db5606fd9492524ee6b6040e
babynus <support@projeqtor.org>
2013-02-23 23:29:06 +0000

    V3.2.0

--- branches/V3.2/projector/index.php
+++ branches/V3.2/projector/index.php
@@ -1,7 +1,7 @@
 <?php
 /* ============================================================================
  * Default page. Redirects to view directory
- */ 
+ */
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" 
   "http://www.w3.org/TR/html4/strict.dtd">
