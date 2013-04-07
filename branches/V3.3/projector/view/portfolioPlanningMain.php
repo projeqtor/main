e8fbc430c516cfff987426614a23bd1d64200e11
babynus <support@projeqtor.org>
2013-04-07 18:07:08 +0000

    V3.3.0

--- branches/V3.3/projector/view/portfolioPlanningMain.php
+++ branches/V3.3/projector/view/portfolioPlanningMain.php
@@ -6,6 +6,7 @@
   scriptLog('   ->/view/portfolioPlanningMain.php');  
 ?>
 <input type="hidden" name="objectClassManual" id="objectClassManual" value="Planning" />
+<input type="hidden" name="portfolioPlanning" id="portfolioPlanning" value="true" />
 <div id="mainDivContainer" class="container" dojoType="dijit.layout.BorderContainer">
   <div id="listDiv" dojoType="dijit.layout.ContentPane" region="top" splitter="true" style="height:60%;">
    <?php include 'portfolioPlanningList.php'?>
