668aad6f4105494c39f4956393f1b9d17076e664
babynus <support@projeqtor.org>
2013-05-11 14:59:08 +0000

    V3.4.0

---
+++ branches/V3.4/projector/db/Projector_V3.4.0.sql
@@ -0,0 +1,18 @@
+
+-- ///////////////////////////////////////////////////////////
+-- // PROJECTOR                                             //
+-- //-------------------------------------------------------//
+-- // Version : V3.4.0                                      //
+-- // Date : 2013-05-06                                     //
+-- ///////////////////////////////////////////////////////////
+--
+--
+
+INSERT INTO `${prefix}parameter` (idUser, idProject, parameterCode, parameterValue) VALUES
+(null,null, 'realWorkOnlyForResponsible', 'NO');
+
+ALTER TABLE `${prefix}type` ADD COLUMN `description` varchar(4000);
+
+ALTER TABLE `${prefix}expensedetailtype` ADD COLUMN `description` varchar(4000);
+
+ALTER TABLE `${prefix}contexttype` ADD COLUMN `description` varchar(4000);
