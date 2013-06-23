8e89c01d82215b7b50b8f9df6b9a5082535444f1
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2013-06-23 21:02:16 +0000

    V3.4.0

--- branches/V3.4/projector/external/dojo/node.js
+++
@@ -1,2 +0,0 @@
-//>>built
-define("dojo/node",["./has"],function(_1){if(!0){throw new Error("node plugin failed to load because environment is not Node.js");}return {load:function(id,_2,_3){if(!_2.nodeRequire){throw new Error("Cannot find native require function");}_3((function(id,_4){var _5=define,_6;define=undefined;try{_6=_4(id);}finally{define=_5;}return _6;})(id,_2.nodeRequire));},normalize:function(id){if(id.charAt(0)==="."){id=require.baseUrl+id;}return id;}};});
