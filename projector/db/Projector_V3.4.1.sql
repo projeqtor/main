f7df40e6e5577a0c5812c240706b468689535a6b
babynus <support@projeqtor.org>
2013-07-25 20:25:27 +0000

    V3.4.1

---
+++ projector/db/Projector_V3.4.1.sql
@@ -0,0 +1,15 @@
+
+-- ///////////////////////////////////////////////////////////
+-- // PROJECTOR                                             //
+-- //-------------------------------------------------------//
+-- // Version : V3.4.0                                      //
+-- // Date : 2013-05-06                                     //
+-- ///////////////////////////////////////////////////////////
+--
+--
+
+UPDATE `${prefix}assignment` SET idProject=(select idProject from `${prefix}activity` A where A.id=refId) 
+where idProject=0 and refType='Activity';
+
+UPDATE `${prefix}assignment` SET idProject=(select idProject from `${prefix}ticket` T where T.id=refId) 
+where idProject=0 and refType='Ticket';
