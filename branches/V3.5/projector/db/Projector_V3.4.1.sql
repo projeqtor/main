591aeae35b3b03235d37e9672ccdb5acec853f07
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2013-07-26 20:41:52 +0000

    V3.5.0

--- branches/V3.5/projector/db/Projector_V3.4.1.sql
+++ branches/V3.5/projector/db/Projector_V3.4.1.sql
@@ -13,3 +13,9 @@ where idProject=0 and refType='Activity';
 
 UPDATE `${prefix}assignment` SET idProject=(select idProject from `${prefix}ticket` T where T.id=refId) 
 where idProject=0 and refType='Ticket';
+
+UPDATE `${prefix}work` SET idProject=(select idProject from `${prefix}activity` A where A.id=refId) 
+where idProject=0 and refType='Activity';
+
+UPDATE `${prefix}work` SET idProject=(select idProject from `${prefix}ticket` T where T.id=refId) 
+where idProject=0 and refType='Ticket';
