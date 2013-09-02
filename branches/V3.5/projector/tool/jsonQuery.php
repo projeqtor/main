1c5a4b41001181a3e825e94297f97acb8dd22f7a
babynus <support@projeqtor.org>
2013-09-02 21:07:15 +0000

    V3.5.0

--- branches/V3.5/projector/tool/jsonQuery.php
+++ branches/V3.5/projector/tool/jsonQuery.php
@@ -80,7 +80,7 @@
     if ($objectClass=='Project' and $accessRightRead!='ALL') {
         $accessRightRead='ALL';
         $queryWhere.= ($queryWhere=='')?'':' and ';
-        $queryWhere.=  '(' . $table . ".id in " . transformListIntoInClause($_SESSION['user']->getVisibleProjects()) ;
+        $queryWhere.=  '(' . $table . ".id in " . transformListIntoInClause($_SESSION['user']->getVisibleProjects(! $showIdle)) ;
         if ($objectClass=='Project') {
           $queryWhere.= " or codeType='TMP' ";
         }
