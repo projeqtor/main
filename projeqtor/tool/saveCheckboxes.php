9bf2ecaa7bec86bd1697101f5a5fcc262e5bdbde
babynus <support@projeqtor.org>
2013-10-06 20:54:55 +0000

    V4.0.0

--- projeqtor/tool/saveCheckboxes.php
+++ projeqtor/tool/saveCheckboxes.php
@@ -2,40 +2,32 @@
 /** ===========================================================================
  * This script stores checkboxes' states
  */
-require_once "../tool/projector.php";
+require_once "../tool/projeqtor.php";
 scriptLog('   ->/tool/saveCheckboxes.php');
 header("Content-Type: text/plain"); 
 $toStore = (isset($_GET["toStore"])) ? $_GET["toStore"] : NULL;
 $toStore=explode(";",$toStore);
 
+Sql::beginTransaction();
 if(count($toStore)>1){
 	$objClass = $toStore[0];
-	$idUser = $toStore[1];
+	$user=$_SESSION['user'];
+	$idUser = $user->id;
 	unset($toStore[0]);
 	unset($toStore[1]);
-	if(count($toStore)==0) {
-		$toStore='';
-	} else {
-		$toStore=implode(";",$toStore);
+	$cs=new ColumnSelector();
+	$cs->purge("scope='export' and idUser=$idUser and objectClass='$objClass'");
+    foreach ($toStore as $store) {
+		$cs=new ColumnSelector();
+		$cs->scope='export';
+		$cs->idUser=$idUser;
+		$cs->objectClass=$objClass;
+		$cs->field=$store;
+		$cs->hidden=1;
+		$res=$cs->save();
+debugLog($res);
 	}
-	$query='SELECT objclass FROM '.$paramDbPrefix.'printcheckbox WHERE objclass="'.$objClass.'" AND idUser="'.$idUser.'";';
-	$result=Sql::query($query);
-
-	$line = Sql::fetchLine($result);
-	$checkboxDB=array();
-	while($line) {
-		$checkboxDB[]=$line;
-		$line = Sql::fetchLine($result);
-	}
-
-	if(count($checkboxDB)>0){
-		$query ="UPDATE '.$paramDbPrefix.'printcheckbox set checkboxes='".$toStore."' WHERE objclass='".$objClass."' AND idUser='".$idUser."';";
-	} else {
-		$query ="INSERT INTO '.$paramDbPrefix.'printcheckbox (`objclass`,`checkboxes`,`idUser`) VALUES('".$objClass."','".$toStore."','".$idUser."')";
-	}
-	$result=Sql::query($query);
-
-
 }
+Sql::commitTransaction();
 
 ?>
