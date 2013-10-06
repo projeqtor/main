8a5afa99522eb674a070b8c1670ee7280373bbe4
babynus <support@projeqtor.org>
2013-10-06 17:11:40 +0000

    V4.0.0 #953

--- projeqtor/view/main.php
+++ projeqtor/view/main.php
@@ -99,6 +99,9 @@ SqlElement::$_cachedQuery['PlanningElement']=array();
          } else  if (target.id=='dndPlanningColumnSelector') {
         	 showWait();
         	 setTimeout('movePlanningColumn("' + idFrom + '", "' + idTo + '")',100);
+         } else  if (target.id=='dndListColumnSelector') {
+             showWait();
+             setTimeout('moveListColumn("' + idFrom + '", "' + idTo + '")',100);
          } else if (target.id=='dndTodayParameters') {
         	 setTimeout('reorderTodayItems()',100);  
          }
