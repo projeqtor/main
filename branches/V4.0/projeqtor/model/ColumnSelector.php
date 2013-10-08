0779f6137a9377a3f1128f428c62bb7ea51575c6
babynus <support@projeqtor.org>
2013-10-08 21:51:46 +0000

    V4.0.0

--- branches/V4.0/projeqtor/model/ColumnSelector.php
+++ branches/V4.0/projeqtor/model/ColumnSelector.php
@@ -21,7 +21,7 @@ class ColumnSelector extends SqlElement {
   public $_from;
   
   private static $cachedLists=array(); 
-  
+  private static $allFields=false; // Keep it to false as long as addAllFields() is not fiabilized
    /** ==========================================================================
    * Constructor
    * @param $id the id of the object in the database (null if not stored yet)
@@ -43,12 +43,12 @@ class ColumnSelector extends SqlElement {
 // ============================================================================**********
 // GET STATIC DATA FUNCTIONS
 // ============================================================================**********
-  public static function getColumnsList($classObj) {
+  public static function getColumnsList($classObj) {	
   	// scope = "list"
-  	if (isset($cachedLists['list#'.$classObj])) {
-  		return $cachedLists['list#'.$classObj];
+  	if (isset(self::$cachedLists['list#'.$classObj])) {
+  		return self::$cachedLists['list#'.$classObj];
   	}
-    // retrieve from database
+    // retrieve from database, in correct order
   	$user=$_SESSION['user'];
   	$cs=new ColumnSelector();
   	$crit=array('scope'=>'list', 'objectClass'=>$classObj, 'idUser'=>$user->id);
@@ -92,6 +92,9 @@ class ColumnSelector extends SqlElement {
 			  if (!$cs->id) { $cs->save(); }
 			  $result[$field]=$cs;
 		}
+		if (self::$allFields) {
+			$result=self::addAllFields($result,$obj);
+		}
 		foreach ($result as $id=>$cs) {
 			if (! $cs->_name) {
 				if ($cs->id) {
@@ -100,8 +103,85 @@ class ColumnSelector extends SqlElement {
 				unset($result[$id]);
 			}
 		}
-		$cachedLists['list#'.$classObj]=$result;
+		self::$cachedLists['list#'.$classObj]=$result;
     return $result;
+  }
+  
+  private static function addAllFields($result, $obj, $included=false, $sourceClass=null) {
+  // TODO : fiabilize this function
+    $fieldsArray=$obj->getFieldsArray();     
+    $user=$_SESSION['user'];  
+    $cpt=count($result);
+    foreach($obj as $col => $val) {
+      if ( $included and ($col=='id' or $col=='refId' or $col=='refType' or $col=='refName') ) {
+        continue;
+      }
+    	if (substr($col,0,1)=='_') {
+      	continue;
+      }
+      if ($obj->isAttributeSetToField($col,'hidden')) {
+      	continue;
+      }
+      if ($col=="password" or $col=="Origin") {
+        continue;
+      }
+      if (is_object($val)) {
+        $result=self::addAllFields($result, $val, true, get_class($obj));
+        continue;
+      }
+      
+      $dataType = $obj->getDataType($col);
+      $dataLength = $obj->getDataLength($col);
+      if ($dataLength>100) {
+      	continue;
+      } 
+      $cpt++;
+      $cs=new ColumnSelector();       
+      $cs->scope="list";
+      if ($included) {
+      	$cs->_from=get_class($obj);
+      	$cs->objectClass=$sourceClass;
+      } else {	
+        $cs->objectClass=get_class($obj);
+      }    
+      $cs->idUser=$user->id;
+      $cs->field=$col;
+      if (substr($cs->field,0,2)=='id' and strlen($cs->field)>2 and substr($col,2,1)==strtoupper(substr($col,2,1)) ) {
+        $cs->field='name'.substr($cs->field,2);
+      }	
+      if (array_key_exists($cs->field,$result)) {
+      	continue;
+      }
+      $cs->attribute=$col;
+      $cs->sortOrder=$cpt;
+      $cs->widthPct=5;
+      $cs->_name=$col;
+      $cs->_displayName=$obj->getColCaption($col);
+      $cs->_formatter='';
+      $cs->hidden=true;
+      if ($col=='id') {
+        $cs->_formatter="numericFormatter"; 
+      } else if ($dataType=='date' or $dataType=='datetime' or $dataType=='time') {
+        $cs->_formatter="dateFormatter";
+      } else if ($col=='color' and $dataLength == 7 ) {
+        $cs->_formatter="colorFormatter";
+      } else if ($dataType=='int' and $dataLength==1) {
+        $cs->_formatter="booleanFormatter";
+      } else if (substr($col,0,2)=='id' and $dataType=='int' and strlen($col)>2 and substr($col,2,1)==strtoupper(substr($col,2,1)) ) {
+        if($col=='idStatus') {
+          $formatter="colorNameFormatter";
+        } else if($col=='idProfile') {
+          $formatter="translateFormatter";
+        } else {
+          $formatter="nameFormatter";
+        }
+      } else if ($dataType=='int' or $dataType=='decimal') {
+      	$cs->_formatter="numericFormatter"; 
+      }
+      $cs->id=1000000+$cpt;
+      $result[$cs->field]=$cs;
+    }
+  	return $result;
   }
 }
 ?>
