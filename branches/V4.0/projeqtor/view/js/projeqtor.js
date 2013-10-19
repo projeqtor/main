212888630152e9ce7d44907560471cf346127aaf
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2013-10-19 19:52:09 +0000

    V4.0.0

--- branches/V4.0/projeqtor/view/js/projeqtor.js
+++ branches/V4.0/projeqtor/view/js/projeqtor.js
@@ -2254,3 +2254,29 @@ function updateCommandTotal() {
 	dijit.byId("validatedPricePerDayAmount").set("value", validatedPricePerDayAmount);	
 	terminateChange();
 }
+
+function copyDirectLinkUrl() {
+  dojo.byId('directLinkUrlDiv').style.display='block';
+  dojo.byId('directLinkUrlDiv').select();
+  setTimeout("dojo.byId('directLinkUrlDiv').style.display='none';",5000);
+  return false;
+}
+
+/*function copyToClipboard(inElement) {
+  if (inElement.createTextRange) {
+    var range = inElement.createTextRange();
+    if (range && BodyLoaded==1) {
+      range.execCommand('Copy');
+    }
+  } else {
+    var flashcopier = 'flashcopier';
+    if(!document.getElementById(flashcopier)) {
+      var divholder = document.createElement('div');
+	  divholder.id = flashcopier;
+	  document.body.appendChild(divholder);
+	}
+	document.getElementById(flashcopier).innerHTML = '';
+	var divinfo = '<embed src="_clipboard.swf" FlashVars="clipboard='+escape(inElement.value)+'" width="0" height="0" type="application/x-shockwave-flash"></embed>';
+	document.getElementById(flashcopier).innerHTML = divinfo;
+  }
+}*/
