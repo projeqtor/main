27ad3b35d08240f80e634b21b71c65535dea78c1
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2013-10-28 17:23:48 +0000

    Joomla

--- joomla/media/com_acymailing/js/acymailing_module.js
+++ joomla/media/com_acymailing/js/acymailing_module.js
@@ -1,6 +1,6 @@
 /**
  * @package    AcyMailing for Joomla!
- * @version    4.3.4
+ * @version    4.4.1
  * @author     acyba.com
  * @copyright  (C) 2009-2013 ACYBA S.A.R.L. All rights reserved.
  * @license    GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
@@ -268,6 +268,10 @@ function acymailingDisplayAjaxResponse(message, type, formName)
 				'height': '0px',
 				'opacity': 0
 			});
+
+			form.setStyles({
+				'display': 'none'
+			});
 		}
 
 		responseContainer.setStyles({
@@ -280,5 +284,6 @@ function acymailingDisplayAjaxResponse(message, type, formName)
 			'height': newContainerHeight,
 			'opacity': 1
 		});
+
 	}
 }
