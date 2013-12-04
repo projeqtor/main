cc08962ed8fc13d561bb6ca6abc4d57bdb9de9fc
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2013-12-04 17:29:25 +0000

    4.1.0

--- branches/V4.1/projeqtor/api/index.php
+++ branches/V4.1/projeqtor/api/index.php
@@ -1,23 +1,99 @@
 <?php 
 $invalidQuery="query should be ../api/class of object/id of object";
+$cronnedScript=true;
+$batchMode=true;
 require_once "../tool/projeqtor.php";
+$user=new User(); 
+$_SESSION['user']=$user;
+
 IF ($_SERVER['REQUEST_METHOD']=='GET') {
   if (isset($_REQUEST['uri'])) {
     $uri=$_REQUEST['uri'];
-echo $uri;
     $split=explode('/',$uri);
     if (count($split>1)) {
     	$class=ucfirst($split[0]);
+    	$where="1=0";
     	if (class_exists($class)) {
+    		$obj=new $class();
+    		$table=$obj->getDatabaseTableName();
     		if (count($split)==2) {
     			$id=$split[1];
-    			$obj=new $class($id);
-    			echo '{"identifier":"id",' ;
-          echo ' "items":[';
-          echo jsonDumpObj($obj);
-          echo ']';
-          echo ' }';
+    			$where="id=$id";   			
+    		} else {
+    			if ($split[1]=='list') {
+    				if ($split[2]=='all') {
+    					$where="1=1";
+    				} else if ($split[2]=='filter') {
+    					$filterId=$split[3];
+    					$crit=new FilterCriteria();
+    					$critList=$crit->getSqlElementsFromCriteria(array('idFilter'=>$filterId));
+    					$where=(count($critList)>0)?"1=1":"1=0";
+    					$idTab=0;
+    					foreach ($critList as $crit) {
+					      if ($crit->sqlOperator!='SORT') {			
+					      	$split=explode('_', $crit->sqlAttribute);        
+					        $critSqlValue=$crit->sqlValue;
+					        if ($crit->sqlOperator=='IN' and $crit->sqlAttribute=='idProduct') {
+					          $critSqlValue=str_replace(array(' ','(',')'), '', $critSqlValue);
+					          $splitVal=explode(',',$critSqlValue);
+					          $critSqlValue='(0';
+					          foreach ($splitVal as $idP) {
+					            $prod=new Product($idP);
+					            $critSqlValue.=', '.$idP;
+					            $list=$prod->getRecursiveSubProductsFlatList(false, false);
+					            foreach ($list as $idPrd=>$namePrd) {
+					              $critSqlValue.=', '.$idPrd;
+					            }
+					          }         
+					          $critSqlValue.=')';
+					        }
+					        if (count($split)>1 ) {
+					          $externalClass=$split[0];
+					          $externalObj=new $externalClass();
+					          $externalTable = $externalObj->getDatabaseTableName();          
+					          $idTab+=1;
+					          //$externalTableAlias = 'T' . $idTab;
+					          //$queryFrom .= ' left join ' . $externalTable . ' as ' . $externalTableAlias .
+					          // ' on ( ' . $externalTableAlias . ".refType='" . get_class($obj) . "' and " .  $externalTableAlias . '.refId = ' . $table . '.id )';
+					          //$queryWhere.=($queryWhere=='')?'':' and ';
+					          //$queryWhere.=$externalTableAlias . "." . $split[1] . ' ' 
+					          //       . $crit['sql']['operator'] . ' '
+					          //       . $critSqlValue;
+					        } else {
+					          $where.=($where=='')?'':' and ';
+					          $where.="(".$table . "." . $crit->sqlAttribute . ' ' 
+					                     . $crit->sqlOperator
+					                     . $critSqlValue;
+					          if (strlen($crit->sqlAttribute)>=9 
+					          and substr($crit->sqlAttribute,0,2)=='id'
+					          and substr($crit->sqlAttribute,-7)=='Version'
+					          and $crit->sqlOperator=='IN') {
+					            $scope=substr($crit->sqlAttribute,2);
+					            $vers=new OtherVersion();
+					            $where.=" or exists (select 'x' from ".$vers->getDatabaseTableName()." VERS "
+					              ." where VERS.refType='".$objectClass."' and VERS.refId=".$table.".id and scope='".$scope."'"
+					              ." and VERS.idVersion IN ".$critSqlValue
+					              .")";
+					          }
+					          $where.=")";
+					        }
+					      }
+					    }
+            }
+    				
+    			}
     		}
+    		echo '{"identifier":"id",' ;
+        echo ' "items":[';        
+        $list=$obj->getSqlElementsFromCriteria(null,null,$where);
+        $cpt=0;
+        foreach ($list as $obj) {
+        	if ($cpt) echo ",";
+        	$cpt++;
+          echo jsonDumpObj($obj);
+        }
+        echo ']';
+        echo ' }';
     		
     	} else {
     		returnError($invalidQuery);
@@ -41,17 +117,24 @@ function returnError($msg) {
 function jsonDumpObj($obj, $included=false) {
 	$res="";
 	foreach($obj as $fld=>$val) {
-		
 		if (is_object($val)) {
-		  //if ($res!="") { $res.=", ";}
-			//$res.=jsonDumpObj($obj, true);
-		} else if (substr($fld,0,1)=='_') {
-			// Nothing
-		} else if ($obj->isAttributeSetToField($fld, 'hidden')) {
+		  if ($res!="") { $res.=", ";}
+			$res.=jsonDumpObj($val, true);
+		} else if (substr($fld,0,1)=='_'
+		  or $obj->isAttributeSetToField($fld, 'hidden')
+		  or $included and ($fld=='id' or $fld=='refType' or $fld=='refId' or $fld=='refName' 
+		                 or $fld=='handled' or $fld=='done' or $fld=='idle' or $fld=='cancelled') ) {
 			// Nothing
 		} else {
 		  if ($res!="") { $res.=", ";}
 		  $res.='"' . htmlEncode($fld) . '":"' . htmlEncodeJson($val) . '"';
+		  if (substr($fld,0,2)=='id' and strlen($fld)>2) {
+		  	$idclass=substr($fld,2);
+		  	if (strtoupper(substr($idclass,0,1))==substr($idclass,0,1)) {
+		  		$res.=", ";
+		  		$res.='"name' . $idclass . '":"' . htmlEncodeJson(SqlList::getNameFromId($idclass, $val)) . '"';
+		  	}
+		  }
 		}  
 	}
 	return $res;
