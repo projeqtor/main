76cb7b6b84cc7d8b27d40c92f3f494e63bbde917
babynus <support@projeqtor.org>
2013-12-13 16:54:02 +0000

    V4.1.0

--- branches/V4.1/projeqtor/db/projeqtor_V4.1.0.sql
+++ branches/V4.1/projeqtor/db/projeqtor_V4.1.0.sql
@@ -17,3 +17,39 @@ DELETE FROM `${prefix}columnselector` WHERE attribute='requestRefreshProject';
 DELETE FROM `${prefix}workelement` where (refType, refId) in 
 (select refType, refId from (select * from `${prefix}workelement` w) ww group by refType, refId having count(*) > 1)
 and plannedWork is null and realWork is null;
+
+CREATE TABLE `${prefix}quality` (
+  `id` int(12) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100),
+  `color` varchar(7) DEFAULT NULL,
+  `sortOrder` int(3) unsigned DEFAULT NULL,
+  `icon` varchar(100),
+  `idle` int(1) unsigned DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+
+INSERT INTO `${prefix}quality` (`id`, `name`, `color`, `sortOrder`, `idle`, `icon`) VALUES
+(1,'conform','#32CD32',100,0,'smileyGreen.png'),
+(2,'some remarks','#ffd700',200,0,'smileyYellow.png'),
+(3,'not conform','#FF0000',300,0,'smileyRed.png');
+
+CREATE TABLE `${prefix}trend` (
+  `id` int(12) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100),
+  `color` varchar(7) DEFAULT NULL,
+  `sortOrder` int(3) unsigned DEFAULT NULL,
+  `icon` varchar(100),
+  `idle` int(1) unsigned DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+
+INSERT INTO `${prefix}trend` (`id`, `name`, `color`, `sortOrder`, `idle`, `icon`) VALUES
+(1,'increasing','#32CD32',100,0,'trendUp.png'),
+(2,'even','#ffd700',200,0,'trendEven.png'),
+(3,'decreasing','#FF0000',300,0,'trendDown.png');
+
+ALTER TABLE `${prefix}project` ADD COLUMN `idQuality` int(12) unsigned,
+ADD COLUMN `idTrend` int(12) unsigned,
+ADD COLUMN `idSponsor` int(12) unsigned;
+
+ALTER TABLE `${prefix}health` ADD COLUMN `icon` varchar(100);
