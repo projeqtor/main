a5beda411d88266092d2a2dd1bb02c62df89b36c
babynus <support@projeqtor.org>
2013-12-14 12:26:03 +0000

    V4.1.0

--- projeqtor/db/projeqtor_V4.1.-.sql
+++ projeqtor/db/projeqtor_V4.1.-.sql
@@ -33,6 +33,18 @@ INSERT INTO `${prefix}quality` (`id`, `name`, `color`, `sortOrder`, `idle`, `ico
 (2,'some remarks','#ffd700',200,0,'smileyYellow.png'),
 (3,'not conform','#FF0000',300,0,'smileyRed.png');
 
+INSERT INTO `${prefix}menu` (`id`,`name`,`idMenu`,`type`,`sortOrder`,`level`,`idle`) VALUES 
+(128,'menuQuality',36,'object',706,NULL,0);
+
+INSERT INTO `${prefix}habilitation` (`idProfile`, `idMenu`, `allowAccess`) VALUES
+(1, 128, 1),
+(2, 128, 0),
+(3, 128, 0),
+(4, 128, 0),
+(5, 128, 0),
+(6, 128, 0),
+(7, 128, 0);
+
 CREATE TABLE `${prefix}trend` (
   `id` int(12) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100),
@@ -44,9 +56,21 @@ CREATE TABLE `${prefix}trend` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
 
 INSERT INTO `${prefix}trend` (`id`, `name`, `color`, `sortOrder`, `idle`, `icon`) VALUES
-(1,'increasing','#32CD32',100,0,'trendUp.png'),
-(2,'even','#ffd700',200,0,'trendEven.png'),
-(3,'decreasing','#FF0000',300,0,'trendDown.png');
+(1,'increasing','#32CD32',100,0,'arrowUpGreen.png'),
+(2,'even','#ffd700',200,0,'arrowRightGrey.png'),
+(3,'decreasing','#FF0000',300,0,'arrowDownRed.png');
+
+INSERT INTO `${prefix}menu` (`id`,`name`,`idMenu`,`type`,`sortOrder`,`level`,`idle`) VALUES 
+(129,'menuTrend',36,'object',709,NULL,0);
+
+INSERT INTO `${prefix}habilitation` (`idProfile`, `idMenu`, `allowAccess`) VALUES
+(1, 129, 1),
+(2, 129, 0),
+(3, 129, 0),
+(4, 129, 0),
+(5, 129, 0),
+(6, 129, 0),
+(7, 129, 0);
 
 ALTER TABLE `${prefix}project` ADD COLUMN `idQuality` int(12) unsigned,
 ADD COLUMN `idTrend` int(12) unsigned,
