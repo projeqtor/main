c013fa1148d4ad398fedaf429fdd9d1b4a744e15
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2014-01-07 15:17:28 +0000

    4.1.1

--- branches/V4.1/projeqtor/tool/projeqtor.php
+++ branches/V4.1/projeqtor/tool/projeqtor.php
@@ -4,8 +4,8 @@ session_start();              // Setup session. Must be first command.
 // === Application data : version, dependencies, about message, ...
 $applicationName="ProjeQtOr"; // Name of the application
 $copyright=$applicationName;  // Copyright to be displayed
-$version="V4.1.0";            // Version of application : Major / Minor / Release
-$build="0092";                // Build number. To be increased on each release
+$version="V4.1.1";            // Version of application : Major / Minor / Release
+$build="0093";                // Build number. To be increased on each release
 $website="http://www.projeqtor.org"; // ProjeQtOr site url
 $aboutMessage='';             // About message to be displayed when clicking on application logo
 $aboutMessage.='<div>' . $applicationName . ' ' . $version . ' ('.($build+0).')</div><br/>';
