02e5737e6cab6a9c788a5a35f349556b0748976d
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2014-01-12 22:33:25 +0000

    V4.2.0

--- branches/V4.2/projeqtor/files/config/parameters.php
+++ branches/V4.2/projeqtor/files/config/parameters.php
@@ -4,7 +4,7 @@
 // on 2013-11-02 17:29:47
 // =======================================================================================
 $paramDbType='mysql'; $paramDbPort='3306'; $paramDbUser='root'; $paramDbPassword='mysql';
-$paramDbName='projeqtor_v41';$paramDbPrefix='';
+$paramDbName='projeqtor_v42';$paramDbPrefix='';
 $paramDbHost='127.0.0.1';
 $logFile='../files/logs/projeqtor_${date}.log';
 $logLevel='3';
