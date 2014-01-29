4953dd070e5948541b94540a1e3cd963d7a4d4c5
babynus <support@projeqtor.org>
2014-01-29 12:14:42 +0000

    4.2.0

--- branches/V4.1/projeqtor/tool/setAlertRead.php
+++ branches/V4.1/projeqtor/tool/setAlertRead.php
@@ -12,7 +12,7 @@ if (array_key_exists('remind',$_REQUEST)) {
   $remind=$_REQUEST['remind'];
 }
 Sql::beginTransaction();
-$idAlert=$_REQUEST['idAlert'];
+$idAlert=trim($_REQUEST['idAlert']);
 $alert=new Alert($idAlert);
 if ($remind) {
 	$alert->alertDateTime= (addDelayToDatetime(date('Y-m-d H:i'), ($remind/60), 'HH'));
