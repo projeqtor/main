ef83f697758d05d8b34ba1565be62301672ac067
babynus <support@projeqtor.org>
2014-02-10 22:45:25 +0000

    V4.2.0

--- branches/V4.2/projeqtor/tool/dynamicDialogChecklist.php
+++ branches/V4.2/projeqtor/tool/dynamicDialogChecklist.php
@@ -1,4 +1,7 @@
 <?php
+if (! isset ($print)) {
+	$print=false;
+}
 if (! array_key_exists('objectClass',$_REQUEST)) {
 	throwError('Parameter objectClass not found in REQUEST');
 }
@@ -54,14 +57,17 @@ foreach ($linesTmp as $line) {
 
 $canUpdate=(securityGetAccessRightYesNo('menu' . $objectClass, 'update', $obj)=='YES');
 if ($obj->idle) $canUpdate=false;
+if ($print) $canUpdate=false;
 ?>
+<?php if (! $print) {?>
 <form id="dialogChecklistForm" name="dialogChecklistForm" action="">
 <input type="hidden" name="checklistDefinitionId" value="<?php echo $checklistDefinition->id;?>" />
 <input type="hidden" name="checklistId" value="<?php echo $checklist->id;?>" />
 <input type="hidden" name="checklistObjectClass" value="<?php echo $objectClass;?>" />
 <input type="hidden" name="checklistObjectId" value="<?php echo $objectId;?>" />
+<?php }?>
 <table style="width: 100%;">
-  <tr><td>
+  <tr><td style="width: 100%;">
 	  <table width="100%">
 <?php foreach($defLines as $line) {
 	      if (isset($linesVal[$line->id])) {
@@ -71,7 +77,7 @@ if ($obj->idle) $canUpdate=false;
         }?>	 
 		  <tr>
 <?php if ($line->check01) {?>
-			  <td class="noteData" style="position: relative; border-right:0; text-align:right" title="<?php echo $line->title;?>"> 
+			  <td class="noteData" style="position: relative; border-right:0; text-align:right" title="<?php echo ($print)?'':$line->title;?>"> 
 				  <?php echo htmlEncode( $line->name);?> :   
 		    </td>
 			  <td class="noteData" style="border-left:0;">
@@ -80,16 +86,24 @@ if ($obj->idle) $canUpdate=false;
 						$check='check0'.$i;
 						$title='title0'.$i;
 						$value='value0'.$i;?>
-						<td style="min-width:100px; white-space:nowrap; vertical-align:top;" title="<?php echo $line->$title;?>" >
+						<td style="min-width:100px; white-space:nowrap; vertical-align:top;" title="<?php echo ($print)?'':$line->$title;?>" >
 						<?php if ($line->$check) {
-						  $checkName="check_".$line->id."_".$i;?>
+						  $checkName="check_".$line->id."_".$i;
+						  if ($print) {
+                $checkImg="checkedKO.png";
+                if ($lineVal->$value) {
+	               $checkImg= 'checkedOK.png';
+                }
+                echo '<img src="img/' . $checkImg . '" />&nbsp;'.$line->$check.'&nbsp;&nbsp;';
+					    } else {?>
 						  <div dojoType="dijit.form.CheckBox" type="checkbox"
-				        <?php if ($line->exclusive) {?>onClick="checkClick(<?php echo $line->id;?>, <?php echo $i;?>)" <?php }?>
+				        <?php if ($line->exclusive and ! $print) {?>onClick="checkClick(<?php echo $line->id;?>, <?php echo $i;?>)" <?php }?>
 				        name="<?php echo $checkName;?>" id="<?php echo $checkName;?>"
 				        <?php if (! $canUpdate) echo 'readonly';?>
 		            <?php if ($lineVal->$value) { echo 'checked'; }?> ></div>
 						    <span style="cursor:pointer;" onClick="dojo.byId('<?php echo $checkName;?>').click();"><?php echo $line->$check;?>&nbsp;&nbsp;</span>
-					  <?php }?></td>
+					  <?php } 
+               }?></td>
 					<?php }?>
 				  </tr></table>
 				</td>
@@ -106,9 +120,10 @@ if ($obj->idle) $canUpdate=false;
 	    </tr>
 	  </table>
   </td></tr>
- <tr><td>&nbsp;</td></tr>
+ <tr><td style="width: 100%;">&nbsp;</td></tr>
+<?php if (! $print) {?>
  <tr>
-   <td align="center">
+   <td style="width: 100%;" align="center">
      <button dojoType="dijit.form.Button" type="button" onclick="dijit.byId('dialogChecklist').hide();">
        <?php echo i18n("buttonCancel");?>
      </button>
@@ -118,5 +133,6 @@ if ($obj->idle) $canUpdate=false;
      </button>
    </td>
  </tr>      
+<?php }?> 
 </table>
-</form>
+<?php if (! $print) {?></form><?php }?>
