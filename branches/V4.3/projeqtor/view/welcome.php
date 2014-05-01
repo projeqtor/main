30ecfa08e2c0a2cb62ceffc12b5ca585015e4d9b
babynus <support@projeqtor.org>
2014-05-01 14:26:27 +0000

    V4.3.0

--- branches/V4.3/projeqtor/view/welcome.php
+++ branches/V4.3/projeqtor/view/welcome.php
@@ -12,8 +12,9 @@
             <img style="height:50%;top:25%;left:25%;opacity:0.10;filter:alpha(opacity=10);" src="img/logoBig.png" />
           </div>
           <div id="welcomeTitle" style="position:absolute;width:100%;height:100%;top:5%;left:-30%" >
-            <?php $logo="img/title.gif"; 
-                  if (file_exists("../logo.gif")) $logo="../logo.gif"; ?> 
+            <?php $logo="img/title.png"; 
+                  if (file_exists("../logo.gif")) $logo="../logo.gif"; 
+                  if (file_exists("../logo.png")) $logo="../logo.png"; ?> 
             <img src="<?php echo $logo;?>" style="width: 300px; height:54px"/>
           </div>
         </div>
