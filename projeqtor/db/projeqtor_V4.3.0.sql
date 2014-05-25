a948b3b942d7b63812cdcd953365ea97bad773f4
babynus <support@projeqtor.org>
2014-05-25 19:25:23 +0000

    V4.3.0

--- projeqtor/db/projeqtor_V4.3.0.sql
+++ projeqtor/db/projeqtor_V4.3.0.sql
@@ -5,36 +5,6 @@
 -- // Date : 2014-03-06                                     //
 -- ///////////////////////////////////////////////////////////
 
-INSERT INTO `${prefix}menu` (`id`, `name`, `idMenu`, `type`, `sortOrder`, `level`, `idle`) VALUES 
-(133, 'menuDiary', '7', 'item', '145', null, 0);
-
-INSERT INTO `${prefix}habilitation` (`idProfile`, `idMenu`, `allowAccess`) VALUES
-(1, 133, 1),
-(2, 133, 1),
-(3, 133, 1),
-(4, 133, 1),
-(5, 133, 0),
-(6, 133, 0),
-(7, 133, 0);
-
-INSERT INTO `${prefix}habilitationother` (idProfile,scope,rightAccess) VALUES 
-(1,'diary','4'),
-(2,'diary','2'),
-(3,'diary','3'),
-(4,'diary','2'),
-(6,'diary','1'),
-(7,'diary','1'),
-(5,'diary','1');
-
-INSERT INTO `${prefix}habilitationother` (idProfile,scope,rightAccess) VALUES 
-(1,'resourcePlanning','1'),
-(2,'resourcePlanning','1'),
-(3,'resourcePlanning','1'),
-(4,'resourcePlanning','2'),
-(6,'resourcePlanning','2'),
-(7,'resourcePlanning','2'),
-(5,'resourcePlanning','2');
-
 UPDATE `${prefix}project` p SET 
 sortOrder=(select wbsSortable from `${prefix}planningelement` pe where refType='Project' and refId=p.id);
 
