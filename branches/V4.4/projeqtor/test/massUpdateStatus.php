3a83353af852f376a5f0dc6c7b1a156edecd5473
babynus <support@projeqtor.org>
2014-06-12 13:59:55 +0000

    4.4.0

---
+++ branches/V4.4/projeqtor/test/massUpdateStatus.php
@@ -0,0 +1,18 @@
+<?php
+include_once '../tool/projeqtor.php';
+$class=$_REQUEST['class'];
+$from=$_REQUEST['statusFrom'];
+$to=$_REQUEST['statusTo'];
+
+$obj=new $class();
+
+projeqtor_set_time_limit(600);
+$crit=array('idStatus'=>$from);
+$lst=$obj->getSqlElementsFromCriteria($crit);
+foreach ($lst as $obj) {
+	Sql::beginTransaction();
+	$obj->idStatus=$to;
+	$obj->save();
+	echo $class . " #" . $obj->id . " - ". $obj->name . '<br>';
+	Sql::commitTransaction();
+}
