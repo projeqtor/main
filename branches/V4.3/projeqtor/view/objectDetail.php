5e1b58d37c03d5723bf631e9fdc7ec60f43e362a
babynus <support@projeqtor.org>
2014-07-08 22:39:39 +0000

    V4.3.3

--- branches/V4.3/projeqtor/view/objectDetail.php
+++ branches/V4.3/projeqtor/view/objectDetail.php
@@ -2812,7 +2812,9 @@ if ( array_key_exists('refresh',$_REQUEST) ) {
   if ( $noselect) {
   	echo $noData;
   } else if (!$canRead) {
-    echo htmlGetNoAccessMessage($objClass);;
+    echo htmlGetNoAccessMessage($objClass);
+    echo "</div></form>";
+    exit;
   } else {
   	if (! $print or $comboDetail) {
   		echo '<input type="hidden" id="className" name="className" value="' . $objClass . '" />' . $cr;
