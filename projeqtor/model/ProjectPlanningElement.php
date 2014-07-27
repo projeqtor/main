8e5f7f2faa15545c59245e898267f86e307f532c
babynus <support@projeqtor.org>
2014-07-27 21:14:55 +0000

    V4.4.0

--- projeqtor/model/ProjectPlanningElement.php
+++ projeqtor/model/ProjectPlanningElement.php
@@ -10,31 +10,37 @@ class ProjectPlanningElement extends PlanningElement {
   public $refType;
   public $refId;
   public $refName;
-  public $_tab_8_5 = array('requested', 'validated', 'assigned', 'planned', 'real', 'left', '', '', 'startDate', 'endDate', 'duration', 'work', 'cost');
+  public $_tab_10_6 = array('requested', 'validated', 'assigned', 'planned', 'real', 'left', '', '', '', '', 'startDate', 'endDate', 'duration', 'work', 'cost', 'expense');
   public $initialStartDate;
   public $validatedStartDate;
   public $_void_13;
   public $plannedStartDate;
   public $realStartDate;
-  public $_void_15;
+  public $_void_16;
   public $_label_priority;
   public $priority;
+  public $_void_19;
+  public $_void_10;
   public $initialEndDate;
   public $validatedEndDate;
   public $_void_23;
   public $plannedEndDate;
   public $realEndDate;
-  public $_void_25;
   public $_void_26;
   public $_void_27;
+  public $_void_28;
+  public $_void_29;
+  public $_void_20;
   public $initialDuration;
   public $validatedDuration;
   public $_void_33;
   public $plannedDuration;
   public $realDuration;
-  public $_void_35;
+  public $_void_36;
   public $_label_wbs;
   public $wbs;
+  public $_void_39;
+  public $_void_30;
   public $_void_41;
   public $validatedWork;
   public $assignedWork;
@@ -43,14 +49,29 @@ class ProjectPlanningElement extends PlanningElement {
   public $leftWork;
   public $_label_progress;
   public $progress;
+  public $_label_expected;
+  public $expectedProgress;
   public $_void_51;
   public $validatedCost;
   public $assignedCost;
   public $plannedCost;
   public $realCost;
   public $leftCost;
-  public $_label_expected;
-  public $expectedProgress;
+  public $_void_57;
+  public $_void_58;
+  public $_void_59;
+  public $_void_50;
+  public $_void_61;
+  public $_void_62;
+  public $assignedExpense;
+  public $plannedExpense;
+  public $realExpense;
+  public $leftExpense;
+  public $_void_67;
+  public $_void_68;
+  public $_void_69;
+  public $_void_60;
+  
   public $wbsSortable;
   public $topId;
   public $topRefType;
@@ -68,7 +89,11 @@ class ProjectPlanningElement extends PlanningElement {
     "realWork"=>"readonly,noImport",
     "leftWork"=>"readonly,noImport",
     "assignedWork"=>"readonly,noImport",
-    "idPlanningMode"=>"hidden,noImport"
+    "idPlanningMode"=>"hidden,noImport",
+  	"assignedExpense"=>"readonly,noImport",
+  	"plannedExpense"=>"readonly,noImport",
+  	"realExpense"=>"readonly,noImport",
+  	"leftExpense"=>"readonly,noImport"
   );   
   
   private static $_databaseTableName = 'planningelement';
--- projeqtor/model/TestSessionPlanningElement.php
+++ projeqtor/model/TestSessionPlanningElement.php
@@ -10,21 +10,23 @@ class TestSessionPlanningElement extends PlanningElement {
   public $refType;
   public $refId;
   public $refName;
-  public $_tab_8_5 = array('requested', 'validated', 'assigned', 'planned', 'real', 'left', '', '', 'startDate', 'endDate', 'duration', 'work', 'cost');
+  public $_tab_10_5 = array('requested', 'validated', 'assigned', 'planned', 'real', 'left', '', '', '', '', 'startDate', 'endDate', 'duration', 'work', 'cost');
   public $initialStartDate;
   public $validatedStartDate;
   public $_void_13;
   public $plannedStartDate;
   public $realStartDate;
-  public $_void_15;
+  public $_void_16;
   public $_label_priority;
   public $priority;
+  public $_void_19;
+  public $_void_10;
   public $initialEndDate;
   public $validatedEndDate;
   public $_void_23;
   public $plannedEndDate;
   public $realEndDate;
-  public $_void_25;
+  public $_void_26;
   public $_label_planning;
   public $idTestSessionPlanningMode;
   public $initialDuration;
@@ -32,9 +34,11 @@ class TestSessionPlanningElement extends PlanningElement {
   public $_void_33;
   public $plannedDuration;
   public $realDuration;
-  public $_void_35;
+  public $_void_36;
   public $_label_wbs;
   public $wbs;
+  public $_void_39;
+  public $_void_30;
   public $_void_41;
   public $validatedWork;
   public $assignedWork;
@@ -43,14 +47,18 @@ class TestSessionPlanningElement extends PlanningElement {
   public $leftWork;
   public $_label_progress;
   public $progress;
+  public $_label_expected;
+  public $expectedProgress;
   public $_void_51;
   public $validatedCost;
   public $assignedCost;
   public $plannedCost;
   public $realCost;
   public $leftCost;
-  public $_label_expected;
-  public $expectedProgress;
+  public $_void_57;
+  public $_void_58;
+  public $_void_59;
+  public $_void_50;
   public $wbsSortable;
   public $topId;
   public $topRefType;
@@ -69,7 +77,7 @@ class TestSessionPlanningElement extends PlanningElement {
     "realWork"=>"readonly,noImport",
     "leftWork"=>"readonly,noImport",
     "assignedWork"=>"readonly,noImport",
-    "idTestSessionPlanningMode"=>"required,mediumWidth  ",
+    "idTestSessionPlanningMode"=>"required,mediumWidth,colspan3",
     "idPlanningMode"=>"hidden,noImport"
   );   
   
