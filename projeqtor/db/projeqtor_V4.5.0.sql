3852d73b36cabe4ecf9f81cab1405d13d3e7df11
babynus <support@projeqtor.org>
2014-10-05 18:36:20 +0000

    V4.5.0

--- projeqtor/db/projeqtor_V4.5.0.sql
+++ projeqtor/db/projeqtor_V4.5.0.sql
@@ -7,3 +7,6 @@
 
 UPDATE `${prefix}accessprofile` SET name='accessProfileRestrictedReader' WHERE name='accessProfileRestrictiedReader';
 UPDATE `${prefix}accessprofile` SET name='accessProfileRestrictedCreator' WHERE name='accessProfileRestricedCreator';
+
+INSERT INTO `${prefix}dependable` (id, `name`, `scope`, `idDefaultDependable`, `idle`) VALUES 
+(7, 'Meeting', 'PE', 1, 0);
