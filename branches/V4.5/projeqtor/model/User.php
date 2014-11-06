2ca2e9e92f4045d298a6b4d4947cb134c0cd6624
babynus <support@projeqtor.org>
2014-11-06 23:26:05 +0000

    V4.5.0

--- branches/V4.5/projeqtor/model/User.php
+++ branches/V4.5/projeqtor/model/User.php
@@ -294,7 +294,7 @@ class User extends SqlElement {
           $result.='<td class="label">'.i18n('colPhoto').'&nbsp;:&nbsp;</td>';
           $result.='<td>&nbsp;&nbsp;';
           $result.='<img src="css/images/smallButtonRemove.png" onClick="removeAttachement('.$image->id.');" title="'.i18n('removePhoto').'" class="smallButton"/>';         
-        	$horizontal='right:52%';
+        	$horizontal='right:51%';
     	    $top='30px';
     	  } else {
     	  	if ($outMode=='pdf') {
@@ -321,7 +321,7 @@ class User extends SqlElement {
           $result.='<td class="label">'.i18n('colPhoto').'&nbsp;:&nbsp;</td>';
           $result.='<td>&nbsp;&nbsp;';
           $result.='<img src="css/images/smallButtonAdd.png" onClick="addAttachement(\'file\');" title="'.i18n('addPhoto').'" class="smallButton"/> ';
-          $result.='<div style="position: absolute; top:'.$top.'px;left:'.$left.'px; width:80px;height:80px;border: 1px solid grey;color: grey;font-size:80%; text-align:center;cursor: pointer;" '
+          $result.='<div style="position: absolute; top:30px;right:51%; width:80px;height:80px;border: 1px solid grey;color: grey;font-size:80%; text-align:center;cursor: pointer;" '
               .' onClick="addAttachement(\'file\');" title="'.i18n('addPhoto').'">'
               . i18n('addPhoto').'</div>';
           $result.='</td>';
