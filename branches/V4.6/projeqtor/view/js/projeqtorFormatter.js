4f60a80d8fc5afec0d2f9f2df38294f2c33addd3
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2014-12-02 15:27:58 +0000

    V4.5.3 ?

--- branches/V4.6/projeqtor/view/js/projeqtorFormatter.js
+++ branches/V4.6/projeqtor/view/js/projeqtorFormatter.js
@@ -216,6 +216,7 @@ function numericFormatter(value) {
 }
 
 function workFormatter(value) {
+	  if (value==null) return null;
 	  // result=dojo.number.format(value);
 	  if (top.paramWorkUnit!='days') value=value*top.paramHoursPerDay;
 	  roundedValue=dojo.number.format(Math.round(value*100)/100);
