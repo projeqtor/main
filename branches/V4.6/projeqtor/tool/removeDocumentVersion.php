0ce2d63a4ba2baaa7b34fc44ae66c3078ba7dc64
babynus <support@projeqtor.org>
2014-12-15 16:44:30 +0000

    V4.6.0 GUI Improvment

--- branches/V4.6/projeqtor/tool/removeDocumentVersion.php
+++ branches/V4.6/projeqtor/tool/removeDocumentVersion.php
@@ -50,14 +50,5 @@ if (file_exists($file)) {
 $result=$obj->delete();
 
 // Message of correct saving
-if (stripos($result,'id="lastOperationStatus" value="ERROR"')>0 ) {
-	Sql::rollbackTransaction();
-  echo '<span class="messageERROR" >' . $result . '</span>';
-} else if (stripos($result,'id="lastOperationStatus" value="OK"')>0 ) {
-	Sql::commitTransaction();
-  echo '<span class="messageOK" >' . $result . '</span>';
-} else { 
-	Sql::rollbackTransaction();
-  echo '<span class="messageWARNING" >' . $result . '</span>';
-}
+displayLastOperationStatus($result);
 ?>
