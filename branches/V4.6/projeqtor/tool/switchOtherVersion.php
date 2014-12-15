808d33dee9af78db1cc0c4b6bc7d3f1848955d5a
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2014-12-15 16:48:31 +0000

    V4.6.0 GUI Improvment

--- branches/V4.6/projeqtor/tool/switchOtherVersion.php
+++ branches/V4.6/projeqtor/tool/switchOtherVersion.php
@@ -65,14 +65,5 @@ $vers->idUser=$user->id;
 $vers->idVersion=$mainVers;
 $res=$vers->save();
 // Message of correct saving
-if (stripos($result,'id="lastOperationStatus" value="ERROR"')>0 ) {
-	Sql::rollbackTransaction();
-  echo '<span class="messageERROR" >' . $result . '</span>';
-} else if (stripos($result,'id="lastOperationStatus" value="OK"')>0 ) {
-	Sql::commitTransaction();
-  echo '<span class="messageOK" >' . $result . '</span>';
-} else { 
-	Sql::rollbackTransaction();
-  echo '<span class="messageWARNING" >' . $result . '</span>';
-}
+displayLastOperationStatus($result);
 ?>
