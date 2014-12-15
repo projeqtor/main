f5e6f903c24a7111dedff106fd2e4ed45eba8452
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2014-12-15 16:47:24 +0000

    V4.6.0 GUI Improvment

--- branches/V4.6/projeqtor/tool/saveExpenseDetail.php
+++ branches/V4.6/projeqtor/tool/saveExpenseDetail.php
@@ -116,14 +116,5 @@ $expenseDetail->idle=$expense->idle;
 $result=$expenseDetail->save();
 
 // Message of correct saving
-if (stripos($result,'id="lastOperationStatus" value="ERROR"')>0 ) {
-	Sql::rollbackTransaction();
-  echo '<span class="messageERROR" >' . $result . '</span>';
-} else if (stripos($result,'id="lastOperationStatus" value="OK"')>0 ) {
-	Sql::commitTransaction();
-  echo '<span class="messageOK" >' . $result . '</span>';
-} else { 
-	Sql::rollbackTransaction();
-  echo '<span class="messageWARNING" >' . $result . '</span>';
-}
+displayLastOperationStatus($result);
 ?>
