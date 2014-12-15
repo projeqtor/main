a917f71d19d4251ed38627586de535563bc970f9
babynus <support@projeqtor.org>
2014-12-15 16:48:31 +0000

    V4.6.0 GUI Improvment

--- branches/V4.6/projeqtor/tool/saveReportInToday.php
+++ branches/V4.6/projeqtor/tool/saveReportInToday.php
@@ -67,14 +67,5 @@ foreach ($params as $pName=>$pValue) {
 }
 
 // Message of correct saving
-if (stripos($result,'id="lastOperationStatus" value="ERROR"')>0 ) {
-	Sql::rollbackTransaction();
-  echo '<span class="messageERROR" >' . $result . '</span>';
-} else if (stripos($result,'id="lastOperationStatus" value="OK"')>0 ) {
-	Sql::commitTransaction();
-  echo '<span class="messageOK" >' . $result . '</span>';
-} else { 
-	Sql::rollbackTransaction();
-  echo '<span class="messageWARNING" >' . $result . '</span>';
-}
+displayLastOperationStatus($result);
 ?>
--- branches/V4.6/projeqtor/tool/submitWorkPeriod.php
+++ branches/V4.6/projeqtor/tool/submitWorkPeriod.php
@@ -74,14 +74,5 @@ if ($action=='submit') {
 $result=$period->save();
 
 // Message of correct saving
-if (stripos($result,'id="lastOperationStatus" value="ERROR"')>0 ) {
-	Sql::rollbackTransaction();
-  echo '<span class="messageERROR" >' . $result . '</span>';
-} else if (stripos($result,'id="lastOperationStatus" value="OK"')>0 ) {
-	Sql::commitTransaction();
-  echo '<span class="messageOK" >' . $result . '</span>';
-} else { 
-	Sql::rollbackTransaction();
-  echo '<span class="messageWARNING" >' . $result . '</span>';
-}
+displayLastOperationStatus($result);
 ?>
