abeb8de6c33dccbb55820040eff2beda4aee9f1e
babynus <support@projeqtor.org>
2015-01-22 19:47:46 +0000

    V5.0

--- branches/V5.0/projeqtor/deploy/projeqtor.profile.js
+++ branches/V5.0/projeqtor/deploy/projeqtor.profile.js
@@ -15,6 +15,7 @@ dependencies = {
 			    "dojo.dnd.Container",
 			    "dojo.dnd.Manager",
 			    "dojo.dnd.Source",
+			    "dojo.dom-geometry",
 			    "dojo.i18n",
 			    "dojo.parser",
 			    "dojo.store.DataStore",			    
