2f22e5bb69862930cf52a87fbf8608464cd0f0db
babynus <support@projeqtor.org>
2015-06-02 07:27:33 +0000

    V5.0

--- branches/V5.0/projeqtor/files/config/parameters.php
+++ branches/V5.0/projeqtor/files/config/parameters.php
@@ -1,26 +1,29 @@
 <?php
 // =======================================================================================
-// Automatically generated parameter file
-// on 2014-02-13 13:14:30
+// PARAMETERS
 // =======================================================================================
-// MySql Degfault
+
+// ========== Database configuration =====================================================
+// --- MySql Degfault
   $paramDbType='mysql'; $paramDbPort='3306'; $paramDbUser='root'; $paramDbPassword='mysql';
   $paramDbName='projeqtor_v50';$paramDbPrefix='';
-// PostgreSql Default
+// --- PostgreSql Default
 //$paramDbType='pgsql'; $paramDbPort='5432'; $paramDbUser='projeqtor'; $paramDbPassword='projeqtor';
 //$paramDbName='projeqtor';$paramDbPrefix=''; 
-//$paramDbName='steria'; $paramDbPrefix='';
-//$paramDbName='test'; $paramDbPrefix='';
-//$paramDbName='ael'; $paramDbPrefix='ael_';
-$paramDbHost='127.0.0.1';
+$paramDbHost='127.0.0.1';         // With MySql on Windows, better use "127.0.0.1" rather than "localhost"
+$enforceUTF8 = '1';               // Positionned by default for new installs since V4.4.0
+
+// ========== Log file configuration =====================================================
 $logFile='../files/logs/projeqtor_${date}.log';
 $logLevel='3';
-$paramDebugMode='false';
-$enforceUTF8 = '1';
-//$lockPassword="false";
-//$hosted=true;
-$flashReport=true;
-$debugQuery=false;
-$debugPerf=true;
-$debugReport=true;
-$memoryLimitForPDF = '512';
+
+// ========== Contextual configuration ===================================================
+//$lockPassword="false";           // Forbid password change (used in Demo to forbit password change)
+//$hosted=true;                    // Is a hosted mode ? => should hide some configuration (directories, ...)
+$flashReport=true;                 // Specific evolution Parameter
+
+// ========== Debugging configuration ====================================================
+$debugQuery=false;                 // Debug all queries : trace Query and running time for each query
+$debugJsonQuery=false;             // Trace only JsonQuery queries  (retrieving lists)
+$debugPerf=true;                   // Add some timestamps and execution time at all debug lines
+$debugReport=true;                 // Displays report file name on report header 
