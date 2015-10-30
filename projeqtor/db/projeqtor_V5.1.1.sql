308ff747c1bd47e6ae0c8f87a41df77e7d57bc1a
babynus <support@projeqtor.org>
2015-10-30 19:16:10 +0000

    V5.1.1

--- projeqtor/db/projeqtor_V5.1.1.sql
+++ projeqtor/db/projeqtor_V5.1.1.sql
@@ -8,3 +8,6 @@
 DELETE FROM `${prefix}plannedwork` WHERE idProject not in (select id from `${prefix}project`);
 
 DELETE FROM `${prefix}columnselector` WHERE objectClass in ('Project','Quotation','Command','Bill');
+
+INSERT INTO `${prefix}planningmode` (`id`, `applyTo`, `name`, `code`, `sortOrder`, `idle`, `mandatoryStartDate`, `mandatoryEndDate`) VALUES
+(21, 'TestSession', 'PlanningModeSTART', 'START', 130, 0 , 1, 0);
