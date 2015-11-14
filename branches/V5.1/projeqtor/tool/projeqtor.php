b83972f9b720c0aae3456e71fa50211694bb409d
babynus <support@projeqtor.org>
2015-11-14 23:32:02 +0000

    V5.1.3

--- branches/V5.1/projeqtor/tool/projeqtor.php
+++ branches/V5.1/projeqtor/tool/projeqtor.php
@@ -40,8 +40,8 @@ if ( is_session_started() === FALSE ) {
 // === Application data : version, dependencies, about message, ...
 $applicationName = "ProjeQtOr"; // Name of the application
 $copyright = $applicationName; // Copyright to be displayed
-$version = "V5.1.2"; // Version of application : Major / Minor / Release
-$build = "0124"; // Build number. To be increased on each release
+$version = "V5.1.3"; // Version of application : Major / Minor / Release
+$build = "0125"; // Build number. To be increased on each release
 $website = "http://www.projeqtor.org"; // ProjeQtOr site url
 
 /**
