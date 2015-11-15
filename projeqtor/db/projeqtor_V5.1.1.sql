0b5c0b1fe2eb77e812083dc8faa24899602e910c
babynus <support@projeqtor.org>
2015-11-15 10:01:18 +0000

    V5.1.3

--- projeqtor/db/projeqtor_V5.1.1.sql
+++ projeqtor/db/projeqtor_V5.1.1.sql
@@ -6,6 +6,7 @@
 -- ///////////////////////////////////////////////////////////
 
 DELETE FROM `${prefix}plannedwork` WHERE idProject not in (select id from `${prefix}project`);
+DELETE FROM `${prefix}assignment` WHERE idProject not in (select id from `${prefix}project`);
 
 DELETE FROM `${prefix}columnselector` WHERE objectClass in ('Project','Quotation','Command','Bill');
 
