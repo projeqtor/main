1e1cb86e1e090b36f01a9cf594df14315e49d4e6
babynus <support@projeqtor.org>
2016-02-22 17:50:49 +0000

    V5.3.0 #1972

--- projeqtor/db/projeqtor_V5.3.0.sql
+++ projeqtor/db/projeqtor_V5.3.0.sql
@@ -5,7 +5,31 @@
 -- // Date : 2016-02-01                                     //
 -- ///////////////////////////////////////////////////////////
 
+INSERT INTO `${prefix}menu` (`id`, `name`, `idMenu`, `type`, `sortOrder`, `level`, `idle`, `menuClass`) VALUES
+(144, 'menuProductType', 79, 'object', 930, 'ReadWriteType', 0, 'Type '),
+(145, 'menuComponentType', 79, 'object', 932, 'ReadWriteType', 0, 'Type ');
+INSERT INTO `${prefix}habilitation` (`idProfile`, `idMenu`, `allowAccess`) VALUES
+(1, 144, 1),
+(2, 144, 0),
+(3, 144, 0),
+(4, 144, 0),
+(5, 144, 0),
+(6, 144, 0),
+(7, 144, 0),
+(1, 145, 1),
+(2, 145, 0),
+(3, 145, 0),
+(4, 145, 0),
+(5, 145, 0),
+(6, 145, 0),
+(7, 145, 0);
 
+INSERT INTO `${prefix}type` (`scope`, `name`, `sortOrder`, `idle`, `idWorkflow`, `mandatoryDescription`, `mandatoryResultOnDone`, `mandatoryResourceOnHandled`, `lockHandled`, `lockDone`, `lockIdle`, `code`) VALUES 
+('Product', 'software', '10', '0', '1', '0', '0', '0', '0', '0', '0', ''),
+('Product', 'service', '20', '0', '1', '0', '0', '0', '0', '0', '0', ''),
+('Component', 'specific', '10', '0', '1', '0', '0', '0', '0', '0', '0', ''),
+('Component', 'on the shelf', '20', '0', '1', '0', '0', '0', '0', '0', '0', '');
 
-
+ALTER TABLE `${prefix}product` ADD `idProductType` int(12) unsigned DEFAULT NULL;
+ALTER TABLE `${prefix}product` ADD `idComponentType` int(12) unsigned DEFAULT NULL;
 
