8fa8ce871ba2f8beeaa68b113328db1413e626e5
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2016-03-05 09:27:45 +0000

    V5.2.5

--- branches/V5.2/projeqtor/db/projeqtor_V5.2.5.sql
+++ branches/V5.2/projeqtor/db/projeqtor_V5.2.5.sql
@@ -9,3 +9,11 @@ DELETE FROM `${prefix}affectation` WHERE idProject NOT IN (SELECT id FROM `${pre
 
 UPDATE `${prefix}affectation` SET startDate=NULL WHERE startDate='0000-00-00';
 UPDATE `${prefix}affectation` SET endDate=NULL WHERE endDate='0000-00-00';
+
+UPDATE `${prefix}attachment` SET refType='ProductVersion' where refType='Version';
+UPDATE `${prefix}link` SET ref1Type='ProductVersion' where ref1Type='Version';
+UPDATE `${prefix}link` SET ref2Type='ProductVersion' where ref2Type='Version';
+UPDATE `${prefix}note` SET refType='ProductVersion' where refType='Version';
+
+ALTER TABLE `${prefix}projecthistory` CHANGE `realWork` `realWork` DECIMAL(14,5) UNSIGNED;
+ALTER TABLE `${prefix}projecthistory` CHANGE `leftWork` `leftWork` DECIMAL(14,5) UNSIGNED;
