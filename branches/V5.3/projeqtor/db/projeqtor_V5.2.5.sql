4384badbc77a07be345eebc31c8df6cf00cb97f3
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2016-03-07 19:05:09 +0000

    V5.3.0

--- branches/V5.3/projeqtor/db/projeqtor_V5.2.5.sql
+++ branches/V5.3/projeqtor/db/projeqtor_V5.2.5.sql
@@ -6,6 +6,8 @@
 -- ///////////////////////////////////////////////////////////
 
 DELETE FROM `${prefix}affectation` WHERE idProject NOT IN (SELECT id FROM `${prefix}project`);
+DELETE FROM `${prefix}assignment` WHERE idProject NOT IN (SELECT id FROM `${prefix}project`);
+DELETE FROM `${prefix}work` WHERE idProject NOT IN (SELECT id FROM `${prefix}project`);
 
 UPDATE `${prefix}affectation` SET startDate=NULL WHERE startDate='0000-00-00';
 UPDATE `${prefix}affectation` SET endDate=NULL WHERE endDate='0000-00-00';
