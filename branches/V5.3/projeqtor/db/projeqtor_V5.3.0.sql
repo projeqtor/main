6eae74c4c7345087fd08f4556b5a7091bbad59ba
babynus <babynus@f9421885-1815-481d-89b5-6d9585689fe2>
2016-03-23 18:36:04 +0000

    V5.3.0

--- branches/V5.3/projeqtor/db/projeqtor_V5.3.0.sql
+++ branches/V5.3/projeqtor/db/projeqtor_V5.3.0.sql
@@ -8,3 +8,10 @@
 ALTER TABLE `${prefix}restricttype` ADD `idProfile` int(12) unsigned DEFAULT NULL;
 
 CREATE INDEX restricttypeProfile ON `${prefix}restricttype` (idProfile,className,idType);
+
+INSERT INTO `${prefix}report` (`id`, `name`, `idReportCategory`, `file`, `sortOrder`) VALUES
+(57, 'reportPlanDetailPerResource', 2, 'detailPlan.php', 456);
+INSERT INTO `${prefix}reportparameter` (`id`, `idReport`, `name`, `paramType`, `sortOrder`, `defaultValue`) VALUES 
+(169, 57, 'month', 'month', 10, 'currentMonth'),
+(170, 57, 'idProject', 'projectList', 1, 'currentProject'),
+(171, 57, 'idResource', 'resourceList', 20, 'currentResource');
