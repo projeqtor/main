dbdf27c5991faa8e4250dfdb6ef354a12f337ea0
amnedge1331 <gabriel.godard@projeqtor.net>
2016-04-25 15:35:52 +0000

    Correction d'un update qui faisait planter la connection de l'utilisateur

--- projeqtor/db/projeqtor_V5.2.5.sql
+++ projeqtor/db/projeqtor_V5.2.5.sql
@@ -9,8 +9,8 @@ DELETE FROM `${prefix}affectation` WHERE idProject NOT IN (SELECT id FROM `${pre
 DELETE FROM `${prefix}assignment` WHERE idProject NOT IN (SELECT id FROM `${prefix}project`);
 DELETE FROM `${prefix}work` WHERE idProject NOT IN (SELECT id FROM `${prefix}project`);
 
-UPDATE `${prefix}affectation` SET startDate=NULL WHERE startDate='0000-00-00';
-UPDATE `${prefix}affectation` SET endDate=NULL WHERE endDate='0000-00-00';
+--UPDATE `${prefix}affectation` SET startDate=NULL WHERE startDate='0000-00-00';
+--UPDATE `${prefix}affectation` SET endDate=NULL WHERE endDate='0000-00-00';
 
 UPDATE `${prefix}attachment` SET refType='ProductVersion' where refType='Version';
 UPDATE `${prefix}link` SET ref1Type='ProductVersion' where ref1Type='Version';
