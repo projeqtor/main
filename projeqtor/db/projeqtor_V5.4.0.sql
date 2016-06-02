1aebea1ce82cad42b668d7ed309fbc4e187c0e68
babynus <support@projeqtor.org>
2016-06-02 06:33:19 +0000

    V5.4.0 Fix for PostgreSql Compatibility PBE

--- projeqtor/db/projeqtor_V5.4.0.sql
+++ projeqtor/db/projeqtor_V5.4.0.sql
@@ -5,7 +5,8 @@
 -- // Date : 2016-05-12                                     //
 -- ///////////////////////////////////////////////////////////
 
-ALTER TABLE `${prefix}assignment` CHANGE `dailyCost` `dailyCost` NUMERIC(9,2) DEFAULT null,
-CHANGE `newDailyCost` `newDailyCost` NUMERIC(9,2) DEFAULT null;
+ALTER TABLE `${prefix}assignment` CHANGE `dailyCost` `dailyCost` DECIMAL(9,2) UNSIGNED,
+CHANGE `newDailyCost` `newDailyCost` DECIMAL(9,2) UNSIGNED;
+ALTER TABLE `${prefix}billline` CHANGE `quantity` `quantity` DECIMAL(9,2) UNSIGNED;
 ALTER TABLE `${prefix}expense` ADD `idDocument` int(12) unsigned;
 ALTER TABLE `${prefix}filter` ADD `isShared` int(1) unsigned;
